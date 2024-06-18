5db1c12e05953651699a690c0a969490131db527
Axell Padilla <brian.padilla@farinter.com>
2024-06-18 13:48:41 -0600

    another working dbt

--- dbt_kielsa/autoload_dbt_run.py
+++ dbt_kielsa/autoload_dbt_run.py
@@ -2,8 +2,23 @@ import os
 from dotenv import load_dotenv
 import subprocess
 
-# Load environment variables from .env file
-load_dotenv('.env')
+# Define the relative path to the .env file
+env_path = os.path.join(os.path.dirname(__file__), '.env')
 
-# Run dbt debug command
-subprocess.run(['dbt', 'run'])
+# Load environment variables from .env file if it exists
+if os.path.exists(env_path):
+    load_dotenv(env_path)
+
+    # Define paths for --project-dir and --profiles-dir
+    project_dir = os.path.join(os.path.dirname(__file__), '')
+    profiles_dir = os.path.join(os.path.dirname(__file__), '')
+
+    # Run dbt run command with proper directory paths
+    subprocess.run([
+        'dbt', 'run',
+        '--project-dir', project_dir,
+        '--profiles-dir', profiles_dir,
+        '--target', 'dev'
+    ])
+else:
+    print('.env file not found in the script directory. Please create it.')
---
+++ dbt_kielsa/models/dagster_example/my_third_dbt_model.sql
@@ -0,0 +1,6 @@
+
+-- Use the `ref` function to select from other models
+
+select *, 'prueba' as Column_Prueba
+from {{ ref('my_second_dbt_model') }}
+where id = 1
