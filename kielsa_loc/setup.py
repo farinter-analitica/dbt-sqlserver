066e017bd5a48cc52f487e6aafe0d514e6a536f8
Axell Padilla <brian.padilla@farinter.com>
2024-06-19 16:00:49 -0600

    specificvers

--- kielsa_loc/setup.py
+++ kielsa_loc/setup.py
@@ -4,13 +4,13 @@ setup(
     name="kielsa",
     packages=find_packages(exclude=["kielsa_tests"]),
     install_requires=[
-        "dagster",
-        "dbt-core",
-        "dagster-dbt",
+        "dagster==1.7.9",
+        "dbt-core==1.7.16",
+        "dagster-dbt==0.23.9",
         "dagster-cloud",
         "dagster-postgres",
         "pandas",
-        "dbt-sqlserver"
+        "dbt-sqlserver==1.7.4"
     ],
     extras_require={"dev": ["dagster-webserver", "pytest", "pyodbc"]},
 )
