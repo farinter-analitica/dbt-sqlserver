b76628f20519275e570d6eeb8c4530938d4ba07d
Axell Padilla <brian.padilla@farinter.m>
2024-06-19 12:18:31 -0600

    fix path multi os

--- dbt_dwh_farinter/autoload_dbt_debug.py
+++ dbt_dwh_farinter/autoload_dbt_debug.py
@@ -4,7 +4,8 @@ from dotenv import load_dotenv
 import subprocess
 
 # Define the relative path to the .env file
-dbt_project_dir = Path(__file__).joinpath("..").resolve()
+base_os_path = os.path.dirname(__file__)
+dbt_project_dir = Path(base_os_path).joinpath("..").resolve()
 env_path = os.path.join(dbt_project_dir, '.env')
 
 # Load environment variables from .env file if it exists
@@ -13,8 +14,8 @@ if os.path.exists(env_path):
     load_dotenv(env_path)
 
     # Define paths for --project-dir and --profiles-dir
-    project_dir = os.path.join(os.path.dirname(__file__), '')
-    profiles_dir = os.path.join(os.path.dirname(__file__), '')
+    project_dir = os.path.join(base_os_path, '')
+    profiles_dir = os.path.join(base_os_path, '')
 
     # Run dbt run command with proper directory paths
     subprocess.run([
