6eed32fa8666be4f551a57d9c584e0a5aef0def5
Axell Padilla <brian.padilla@farinter.com>
2024-06-25 15:37:29 -0600

    fix tests

--- run_all_tests.py
+++ run_all_tests.py
@@ -1,9 +1,9 @@
 
 import subprocess
-test_dirs = [f"dagster_kielsa_loc\dagster_kielsa_tests"
-             , f"dagster_sap_loc\dagster_sap_tests"
-             , f"dagster_shared_gf_loc\dagster_shared_gf_tests"]
+test_dirs = [f"""dagster_kielsa_loc\dagster_kielsa_tests"""
+             , f"""dagster_sap_loc\dagster_sap_tests"""
+             , f"""dagster_shared_gf_loc\dagster_shared_gf_tests"""]
 
 #run
 for test_dir in test_dirs:
-    subprocess.run(["pytest", f"{test_dir}"])
+    subprocess.run(["pytest", f"{test_dir}","--disable-warnings", "--disable-pytest-warnings"])
