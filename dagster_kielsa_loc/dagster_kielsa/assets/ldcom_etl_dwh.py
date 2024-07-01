300b4213fddcec82e396ce759a12bcf74e2321d1
Axell <68310020+axellpadilla@users.noreply.github.com>
2024-07-01 14:20:44 -0600

    fix logic

--- dagster_kielsa_loc/dagster_kielsa/assets/ldcom_etl_dwh.py
+++ dagster_kielsa_loc/dagster_kielsa/assets/ldcom_etl_dwh.py
@@ -5,14 +5,13 @@ import time
 dl_farinter_assets_prefix = ["DL_FARINTER"]
 @asset(key_prefix= dl_farinter_assets_prefix)
 def DL_Kielsa_Sucursal(dwh_farinter_dl: SQLServerResource) -> None: 
-    result = dwh_farinter_dl.execute_and_commit("EXEC [DL_FARINTER].[dbo].[DL_paCargarKielsa_Sucursal]")
+    dwh_farinter_dl.execute_and_commit("EXEC [DL_FARINTER].[dbo].[DL_paCargarKielsa_Sucursal]")
     #time.sleep(240)
-    return result
+    #return result
 
 @asset(key_prefix= dl_farinter_assets_prefix)
 def DL_Kielsa_Bodega(dwh_farinter_dl: SQLServerResource) -> None: 
-    result = dwh_farinter_dl.execute_and_commit("EXEC [DL_FARINTER].[dbo].[DL_paCargarKielsa_Bodega]")
-    #time.sleep(240)
-    return result
+    dwh_farinter_dl.execute_and_commit("EXEC [DL_FARINTER].[dbo].[DL_paCargarKielsa_Bodega]")
+
 
 
