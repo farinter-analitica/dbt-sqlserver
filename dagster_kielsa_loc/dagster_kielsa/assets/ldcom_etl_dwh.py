da388451a737f012f86224d67de02f86bd0731c9
Axell <68310020+axellpadilla@users.noreply.github.com>
2024-07-01 14:10:51 -0600

    DL_paCargarKielsa_Bodega

--- dagster_kielsa_loc/dagster_kielsa/assets/ldcom_etl_dwh.py
+++ dagster_kielsa_loc/dagster_kielsa/assets/ldcom_etl_dwh.py
@@ -2,9 +2,16 @@ from dagster import asset, AssetKey , AssetExecutionContext
 from dagster_shared_gf.resources.sql_server_resources import SQLServerResource
 import time
 
-@asset(key_prefix= ["DL_FARINTER"])
+dl_farinter_assets_prefix = ["DL_FARINTER"]
+@asset(key_prefix= dl_farinter_assets_prefix)
 def DL_Kielsa_Sucursal(dwh_farinter_dl: SQLServerResource) -> None: 
     result = dwh_farinter_dl.execute_and_commit("EXEC [DL_FARINTER].[dbo].[DL_paCargarKielsa_Sucursal]")
+    #time.sleep(240)
+    return result
+
+@asset(key_prefix= dl_farinter_assets_prefix)
+def DL_Kielsa_Bodega(dwh_farinter_dl: SQLServerResource) -> None: 
+    result = dwh_farinter_dl.execute_and_commit("EXEC [DL_FARINTER].[dbo].[DL_paCargarKielsa_Bodega]")
     #time.sleep(240)
     return result
 
