50e7257a67f5711da75f5405414ede8cbc82a213
Axell <68310020+axellpadilla@users.noreply.github.com>
2024-07-01 23:54:51 -0600

    log clean

--- dagster_kielsa_loc/dagster_kielsa/assets/knime_asset_factory.py
+++ dagster_kielsa_loc/dagster_kielsa/assets/knime_asset_factory.py
@@ -4,6 +4,39 @@ from dagster_shared_gf.shared_functions import get_for_current_env
 from dagster_shared_gf.shared_variables import env_str
 import subprocess
 from typing import List, Dict
+import re
+
+import re
+
+def filter_logs_std(logs):
+    exclude_patterns = [
+        re.compile(r".*ERROR StatusLogger Log4j2 could not find a logging implementation.*", re.IGNORECASE),
+        re.compile(r".*Execution failed in Try-Catch block.*", re.IGNORECASE),
+        re.compile(r"WARN.*Errors overwriting node settings with flow variables.*", re.IGNORECASE),
+        re.compile(r".*Node.*No new variables defined.*", re.IGNORECASE),
+        re.compile(r".*Node.*The node configuration changed.*", re.IGNORECASE),
+        re.compile(r"WARN.*Component does not have input data.*", re.IGNORECASE),
+        re.compile(r"WARN.*No grouping column included.*", re.IGNORECASE),
+        re.compile(r"WARN.*Errors loading flow variables into node.*", re.IGNORECASE),
+        re.compile(r"WARN.*No such variable.*", re.IGNORECASE),
+        re.compile(r"WARN.*The table structures of active ports are not compatible.*", re.IGNORECASE),
+        re.compile(r"WARN.*No aggregation column defined.*", re.IGNORECASE),
+        re.compile(r"WARN.* The input table has fewer rows.*than the specified k.*", re.IGNORECASE),
+        re.compile(r"WARN.*Node All.*partition.*", re.IGNORECASE),
+        re.compile(r"WARN.*Node Multiple inputs are active.*", re.IGNORECASE)
+    ]
+
+    filtered_lines = []
+    for line in logs.split("\n"):
+        line = line.strip()
+        line = re.sub(r"\s+", " ", line)
+
+        if not line:
+            continue
+
+        if not any(pattern.search(line) for pattern in exclude_patterns):
+            filtered_lines.append(" ".join(line.split()))
+    return "\n".join(filtered_lines)
 
 
 # Operation to fetch workflows from the database
@@ -26,6 +59,9 @@ def fetch_knime_workflows(context: OpExecutionContext) -> List[Dict[str, str]]:
             for row in results
         ]
 
+
+
+
 def execute_knime_workflow(knime_bin: str, workflow_directory: str, current_context: AssetExecutionContext | None) -> None:
     if not current_context:
         current_context = AssetExecutionContext.get()
@@ -42,10 +78,10 @@ def execute_knime_workflow(knime_bin: str, workflow_directory: str, current_cont
 
         if process.returncode != 0:
             #raise Exception(f"Workflow execution failed: {stderr.decode('utf-8')}").
-            current_context.log.error(f"Workflow execution failed: {stderr.decode('utf-8')}")
+            current_context.log.error(f"Workflow execution failed: {filter_logs_std(stderr.decode('utf-8'))}")
 
         #return stdout.decode('utf-8')
-        current_context.log.info(stdout.decode('utf-8'))
+        current_context.log.info(filter_logs_std(stdout.decode('utf-8')))
     else:
         current_context.log.info(f"Skipping workflow execution in {env_str} environment. Supported only in {supported_envs} environments.")
     
@@ -88,15 +124,41 @@ def knime_asset_creation_graph():    # first = first_op()     second = second_op
 asset_definitions: list = []
 # The main script to configure and run the job
 if __name__ == "__main__":
-    from dagster_shared_gf.load_env_run import load_env_vars
-    load_env_vars(joinpath_str=["..",".."])
-    from dagster_shared_gf.resources.postgresql_resources import db_analitica_etl
-    # Build the context with the resources
-    #builded_context = build_op_context(resources={"db_analitica_etl":db_analitica_etl})
-    builded_resources = {"db_analitica_etl":db_analitica_etl}
-    # Fetch workflows and create assets
-    asset_definitions = knime_asset_creation_graph.to_job().execute_in_process(resources=builded_resources).output_value()
-    print(asset_definitions)
+    current_test = 2
+    match current_test:
+        case 1:
+            from dagster_shared_gf.load_env_run import load_env_vars
+            load_env_vars(joinpath_str=["..",".."])
+            from dagster_shared_gf.resources.postgresql_resources import db_analitica_etl
+            # Build the context with the resources
+            #builded_context = build_op_context(resources={"db_analitica_etl":db_analitica_etl})
+            builded_resources = {"db_analitica_etl":db_analitica_etl}
+            # Fetch workflows and create assets
+            asset_definitions = knime_asset_creation_graph.to_job().execute_in_process(resources=builded_resources).output_value()
+            assert len(asset_definitions) > 0, "No assets were created for the knnime workflows."
+        case 2:
+            logs = """
+            INFO This is a normal log line.
+            ERROR StatusLogger Log4j2 could not find a logging implementation.
+            Execution failed in Try-Catch block.
+            WARN Errors overwriting node settings with flow variables.
+            Node\t No new variables defined.
+            Node\t The node configuration changed.
+            WARN Component does not have input data.
+            WARN No grouping column included.
+            WARN Errors loading flow variables into node.
+            WARN No such variable.
+            WARN The table structures of active ports are not compatible.
+            WARN No aggregation column defined.
+            WARN The input table has fewer rows 10 than the specified k.
+            WARN Node All partition issues.
+            WARN Node Multiple inputs are active.
+            DEBUG A debug message.
+            INFO Another info message.
+            """
+            expected_logs = """INFO This is a normal log line.\nDEBUG A debug message.\nINFO Another info message."""
+            filtered_logs = filter_logs_std(logs)
+            assert filtered_logs == expected_logs, f"Expected:\n{expected_logs}\nGot:\n{filtered_logs}"       
 else:
     from dagster_shared_gf.resources.postgresql_resources import db_analitica_etl
     builded_resources = {"db_analitica_etl":db_analitica_etl}
