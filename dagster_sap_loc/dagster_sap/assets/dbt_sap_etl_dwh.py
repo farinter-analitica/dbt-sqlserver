b4b4c6b020ca964cdd8c50392bc51ff1a5c449fb
Axell <68310020+axellpadilla@users.noreply.github.com>
2024-07-04 09:35:20 -0600

    fix

--- dagster_sap_loc/dagster_sap/assets/dbt_sap_etl_dwh.py
+++ dagster_sap_loc/dagster_sap/assets/dbt_sap_etl_dwh.py
@@ -4,13 +4,13 @@ from dagster_dbt import DbtCliResource, dbt_assets
 from dagster_shared_gf.resources.dbt_resources import dbt_manifest
 
 class MyDbtConfig(Config):
-    full_refresh: bool = Field(default=False, description="Refresh full dbt models", is_required=False)
+    full_refresh: bool = Field(default_value=False, description="Refresh full dbt models", is_required=False)
 
 @dbt_assets(manifest=dbt_manifest, select="group:sap_etl_dwh")
 def dbt_sap_etl_dwh_assets(context: AssetExecutionContext, dbt_resource: DbtCliResource, config: MyDbtConfig):
     dbt_run_args = ["build"]
     if config.full_refresh:
         dbt_run_args += ["--full-refresh"]
-        context.log.info(context.selected_asset_keys)
+        #context.log.info(context.selected_asset_keys)
     yield from dbt_resource.cli(dbt_run_args, context=context).stream().fetch_row_counts()
 
