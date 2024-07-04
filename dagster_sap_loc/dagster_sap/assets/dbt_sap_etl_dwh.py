469de3b5cd6643cbe343702504250a0a927083df
Axell <68310020+axellpadilla@users.noreply.github.com>
2024-07-03 22:40:23 -0600

    fix "node"

--- dagster_sap_loc/dagster_sap/assets/dbt_sap_etl_dwh.py
+++ dagster_sap_loc/dagster_sap/assets/dbt_sap_etl_dwh.py
@@ -4,6 +4,6 @@ from dagster_dbt import DbtCliResource, dbt_assets
 from dagster_shared_gf.resources.dbt_resources import dbt_manifest
 
 @dbt_assets(manifest=dbt_manifest, select="group:sap_etl_dwh")
-def dbt_dwh_sap_mart_finanzas_assets(context: AssetExecutionContext, dbt_resource: DbtCliResource ):
+def dbt_sap_etl_dwh_assets(context: AssetExecutionContext, dbt_resource: DbtCliResource ):
     yield from dbt_resource.cli(["build"], context=context).stream().fetch_row_counts()
 
