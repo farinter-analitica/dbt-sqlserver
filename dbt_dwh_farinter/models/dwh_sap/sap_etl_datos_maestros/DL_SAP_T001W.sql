05a9e6da79680a3adc089539b595eb376e646ae6
Axell <68310020+axellpadilla@users.noreply.github.com>
2024-07-05 14:49:48 -0600

    FIX T001W

--- dbt_dwh_farinter/models/dwh_sap/sap_etl_datos_maestros/DL_SAP_T001W.sql
+++ dbt_dwh_farinter/models/dwh_sap/sap_etl_datos_maestros/DL_SAP_T001W.sql
@@ -84,6 +84,7 @@ SELECT ISNULL(CAST(A.[WERKS] COLLATE DATABASE_DEFAULT AS VARCHAR(4)),'')  AS [WE
     , ISNULL(CAST(A.[OIHCREDIPI] COLLATE DATABASE_DEFAULT AS VARCHAR(1)),'')  AS [OIHCREDIPI]  --  -Crédito IPI permitido-Check: -Datatype:CHAR-Len:(1,0)
     , ISNULL(CAST(A.[STORETYPE] COLLATE DATABASE_DEFAULT AS VARCHAR(1)),'')  AS [STORETYPE]  --  -Tp.tienda p.diferenc.tienda, gran almacén, tienda convenien.-Check: -Datatype:CHAR-Len:(1,0)
     , ISNULL(CAST(A.[DEP_STORE] COLLATE DATABASE_DEFAULT AS VARCHAR(4)),'')  AS [DEP_STORE]  --  -Grandes almacenes sup.-Check:*-Datatype:CHAR-Len:(4,0)
+    , ISNULL(CAST(S.[BUKRS] COLLATE DATABASE_DEFAULT AS VARCHAR(4)),'')  AS [BUKRS]  --  -Código de empresa-Check: -Datatype:CHAR-Len:(4,0)
     , ISNULL(CAST(GETDATE() AS DATETIME),'1900-01-01') AS [Fecha_Carga]
     , ISNULL(CAST(GETDATE() AS DATETIME),'1900-01-01') AS [Fecha_Actualizado]
 FROM {{ var('linked_server') }}.{{ source('SAPPRD', 'T001W')}} A
