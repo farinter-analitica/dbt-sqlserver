02850ec2d2070fd89d5280cfb0cc8aa492fc7d86
Axell <68310020+axellpadilla@users.noreply.github.com>
2024-07-15 18:35:54 -0600

    mongo sources

--- dagster_kielsa_gf/dagster_kielsa_gf/dlt/definitions.py
+++ dagster_kielsa_gf/dagster_kielsa_gf/dlt/definitions.py
@@ -3,7 +3,7 @@ from dagster_embedded_elt.dlt import DagsterDltResource
 from dagster_kielsa_gf.dlt.assets import mongo_db_crm_etl_dwh
 
 defs = Definitions(
-    assets=mongo_db_crm_etl_dwh.all_mongo_db_hn_assets, # +mongo_db_crm_etl_dwh.all_mongo_db_hn_source_assets,
+    assets=mongo_db_crm_etl_dwh.all_mongo_db_hn_assets + mongo_db_crm_etl_dwh.all_mongo_db_hn_source_assets,
     resources={
         "dlt": DagsterDltResource(),
     },
