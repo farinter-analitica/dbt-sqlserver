c9b7e4e46b6229d1ec4105013b1c0eeea36814bc
Axell <68310020+axellpadilla@users.noreply.github.com>
2024-08-02 17:52:39 -0600

    empleado

---
+++ dbt_dwh_farinter/models/dwh_kielsa/mart_datos_maestros/BI_Kielsa_Dim_Empleado.sql
@@ -0,0 +1,35 @@
+
+{% set unique_key_list = ["Empleado_Id","Emp_Id"] %}
+{{ 
+    config(
+		as_columnstore=false,
+		tags=["periodo/diario"],
+		materialized="incremental",
+        incremental_strategy="farinter_merge",
+		unique_key=unique_key_list,
+		on_schema_change="sync_all_columns",
+		merge_exclude_columns=unique_key_list + ["Fecha_Carga"],
+		merge_check_diff_exclude_columns=unique_key_list + ["Fecha_Carga","Fecha_Actualizado"],
+		post_hook=[
+      "{{ dwh_farinter_remove_incremental_temp_table() }}",
+      "{{ dwh_farinter_create_primary_key(columns=" ~ unique_key_list | tojson ~ ", create_clustered=true, is_incremental=is_incremental(), if_another_exists_drop_it=true) }}",
+      "{{ dwh_farinter_create_dummy_data(unique_key=" ~ unique_key_list | tojson ~ ", is_incremental=0) }}"
+		]
+		
+) }}
+
+SELECT
+	*
+    , ISNULL({{ dwh_farinter_hash_column(unique_key_list) }},'') AS [HashStr_EmplEmp]
+FROM
+	(SELECT 
+			[Empleado_Id]
+			,[Empleado_Nombre]
+			,[Rol_Id]
+			,[Usuario_Id]
+			,[Rol]
+			,[Emp_Id]
+			,[Hash_EmpleadoEmp]
+			,[Sucursal_Id_Asignado_Meta]
+		FROM {{source ('DL_FARINTER','DL_Kielsa_Empleado')}} 
+  ) A
