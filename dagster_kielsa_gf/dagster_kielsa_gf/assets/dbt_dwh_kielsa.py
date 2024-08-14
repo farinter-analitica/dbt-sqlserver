32c3f45909a0fbc277b44b7cba2bff6826c6e11f
Axell <68310020+axellpadilla@users.noreply.github.com>
2024-08-14 16:13:57 -0600

    FULL REFRESH DBT

--- dagster_kielsa_gf/dagster_kielsa_gf/assets/dbt_dwh_kielsa.py
+++ dagster_kielsa_gf/dagster_kielsa_gf/assets/dbt_dwh_kielsa.py
@@ -1,4 +1,4 @@
-from dagster import AssetExecutionContext, load_assets_from_current_module, load_asset_checks_from_current_module, build_last_update_freshness_checks, AssetChecksDefinition,AssetKey
+from dagster import AssetExecutionContext, load_assets_from_current_module, load_asset_checks_from_current_module, build_last_update_freshness_checks, AssetChecksDefinition, Field, Config
 from dagster_dbt import DbtCliResource, dbt_assets, DagsterDbtTranslator
 from typing import Sequence, List, Mapping, Dict, Any
 from datetime import timedelta
@@ -8,9 +8,16 @@ from dagster_shared_gf.resources.dbt_resources import dbt_manifest, MyDbtSourceT
 
 tags_repo = TagsRepositoryGF
 
+
+class MyDbtConfig(Config):
+    full_refresh: bool = Field(default=False, description="Refresh full dbt models")
+
 @dbt_assets(manifest=dbt_manifest, select="tag:dagster_kielsa_gf/dbt", dagster_dbt_translator=MyDbtSourceTranslator())
-def dbt_dwh_kielsa_mart_datos_maestros_assets(context: AssetExecutionContext, dbt_resource: DbtCliResource):
-    yield from dbt_resource.cli(["build"], context=context).stream()
+def dbt_dwh_kielsa_mart_datos_maestros_assets(context: AssetExecutionContext, dbt_resource: DbtCliResource, config: MyDbtConfig):
+    dbt_run_args = ["build"]
+    if config.full_refresh:
+        dbt_run_args += ["--full-refresh"]
+    yield from dbt_resource.cli(dbt_run_args, context=context).stream().fetch_row_counts()
 
 
 all_assets = load_assets_from_current_module()
