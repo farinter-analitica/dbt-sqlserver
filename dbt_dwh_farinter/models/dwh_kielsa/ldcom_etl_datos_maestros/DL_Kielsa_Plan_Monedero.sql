837405cc9cac3d01a5e37c676eee4c5f2f0b472d
Axell <68310020+axellpadilla@users.noreply.github.com>
2024-08-20 11:43:53 -0600

    fix

--- dbt_dwh_farinter/models/dwh_kielsa/ldcom_etl_datos_maestros/DL_Kielsa_Plan_Monedero.sql
+++ dbt_dwh_farinter/models/dwh_kielsa/ldcom_etl_datos_maestros/DL_Kielsa_Plan_Monedero.sql
@@ -7,7 +7,7 @@
 --dbt dagster
 
 SELECT Emp_Id, 
-    Monedero_Id, 
-    Monedero_Nombre, 
+    Monedero_Id as MonederoPlan_Id, 
+    Monedero_Nombre as MonederoPlan_Nombre, 
     ABS(CAST(HASHBYTES('SHA2_256', CONCAT(Monedero_Id, '-', Emp_Id)) AS int)) AS Hash_MonederoPlanEmp
 FROM {{ref ('DL_Kielsa_Monedero_Plan')}}
