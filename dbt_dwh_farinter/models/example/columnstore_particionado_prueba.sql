3a1ecf173d236340350b6c226a3db8c813e3f931
Axell <68310020+axellpadilla@users.noreply.github.com>
2024-09-05 17:40:55 -0600

    dbt sql

--- dbt_dwh_farinter/models/example/columnstore_particionado_prueba.sql
+++ dbt_dwh_farinter/models/example/columnstore_particionado_prueba.sql
@@ -18,7 +18,7 @@
 {{ 
     config(
 		as_columnstore=false,
-		materialized="incremental",
+		materialized="table",
 		incremental_strategy="farinter_merge",
 		unique_key=unique_key_list,
 		on_schema_change="sync_all_columns",
