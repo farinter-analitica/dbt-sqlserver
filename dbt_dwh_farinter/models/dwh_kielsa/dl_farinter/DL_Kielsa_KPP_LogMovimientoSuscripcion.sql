e8e4cf6147b238f6180a5545e5effa94fe59762f
Axell <68310020+axellpadilla@users.noreply.github.com>
2024-09-13 08:57:46 -0600

    KPP_DB Y LOGMOV

---
+++ dbt_dwh_farinter/models/dwh_kielsa/dl_farinter/DL_Kielsa_KPP_LogMovimientoSuscripcion.sql
@@ -0,0 +1,44 @@
+
+{%- set unique_key_list = ["id"] -%}
+{{ 
+    config(
+		as_columnstore=true,
+		tags=["periodo/diario"],
+		materialized="incremental",
+		incremental_strategy="farinter_merge",
+		unique_key=unique_key_list,
+		on_schema_change="sync_all_columns",
+		merge_exclude_columns=unique_key_list + ["Fecha_Carga"],
+		merge_check_diff_exclude_columns=unique_key_list + ["Fecha_Carga","Fecha_Actualizado"],
+		post_hook=[
+      "{{ dwh_farinter_remove_incremental_temp_table() }}",
+      "{{ dwh_farinter_create_primary_key(columns=" ~ unique_key_list | tojson ~ ", create_clustered=false, is_incremental=is_incremental(), if_another_exists_drop_it=true) }}",
+      "{{ dwh_farinter_create_index(is_incremental=is_incremental(), columns=['Fecha_Actualizado']) }}",
+      "{{ dwh_farinter_create_index(is_incremental=is_incremental(), columns=['TarjetaKC_Id']) }}",
+      "{{ dwh_farinter_create_index(is_incremental=is_incremental(), columns=['Sucursal_Registro']) }}",
+		]
+		
+) }}
+
+{% if is_incremental() %}
+	{% set last_date = run_single_value_query_on_relation_and_return(query="""select ISNULL(CONVERT(VARCHAR,DATEADD(DAY, -1, max(Fecha_Actualizado)), 112), '19000101')  from  """ ~ this, relation_not_found_value='00000000'|string)|string %}
+{% else %}
+	{% set last_date = '00000000'|string %}
+{% endif %}
+
+SELECT ISNULL([id],0) AS Id
+      ,ISNULL([TarjetaKC_Id],'') AS TarjetaKC_Id
+      ,ISNULL([Fecha],'19000101') AS [Fecha]
+      ,[CodPlanKielsaClinica]
+      ,[Tipo_Ingreso]
+      ,[Origen]
+      ,[Tipo_Documento]
+      ,[Sucursal_Registro]
+      ,[Usuario_Registro]
+      ,[TipoPlan]
+      ,[Tipo_Registro]
+      , GETDATE() AS Fecha_Actualizado
+  FROM  {{ var('P_SQLLDSUBS_LS') }}.{{ source('KPP_DB', 'LogMovimientoSuscripcion') }} --[KPP_DB].[dbo].[LogMovimientoSuscripcion]
+  {% if is_incremental() %}
+  WHERE Fecha > '{{ last_date }}'
+  {% endif %}
