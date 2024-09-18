d818bab3f1e6cadc80d5fb1c777fe976c0553a9a
Axell <68310020+axellpadilla@users.noreply.github.com>
2024-09-18 11:47:34 -0600

    Fix

--- dbt_dwh_farinter/models/dwh_sap/mart_ventas/BIV_SAP_Hecho_PresupuestoHist_CalcDiario.sql
+++ dbt_dwh_farinter/models/dwh_sap/mart_ventas/BIV_SAP_Hecho_PresupuestoHist_CalcDiario.sql
@@ -18,16 +18,16 @@ SELECT --top 1000
 	, CAL.[Fecha_Calendario] AS Fecha_Id
 	, PH.Monto AS Monto_Mensual
 	, CAST(ROUND(CASE WHEN CAL.Es_dia_Habil = 1 THEN PH.[Monto] * 1.0 
-		/ SUM(CAL.Es_dia_Habil) OVER (PARTITION BY CAL.AnioMes_Id, S.Pais_Id, S.Sociedad_Id)
+		/ CAL.Dias_Laborales
 		ELSE 0.0 END, 2) AS DECIMAL(16, 4)) AS [Monto_Diario]
-	, SUM(CAL.Es_dia_Habil) OVER (PARTITION BY CAL.AnioMes_Id, S.Pais_Id, S.Sociedad_Id) AS Dias_Laborales
+	, CAL.Dias_Laborales
 	, CAL.[AnioMes_Id]
 	, S.Pais_Id
 	, CAL.Es_dia_Habil
 FROM	{{ source ('BI_FARINTER', 'BI_SAP_Hecho_PresupuestoHist') }} PH
 INNER JOIN {{ ref ('BI_SAP_Dim_Sociedad')}} S
 	ON S.Sociedad_Id = PH.Sociedad_Id
-INNER JOIN {{ ref ('BI_Dim_Calendario_LaboralPais')}} CAL
+INNER JOIN (SELECT *, SUM(Es_dia_Habil) OVER (PARTITION BY AnioMes_Id, Pais_ISO2) AS Dias_Laborales FROM {{ ref ('BI_Dim_Calendario_LaboralPais')}}) CAL
 	ON	CAL.[AnioMes_Id] = PH.[AnioMes_Id] 
 	AND CAL.Pais_ISO2 = S.Pais_Id --AND CAL.Sociedad_Id  = S.Sociedad_Id
 --     WHERE PH.[Cliente_Id] = '0000100758'
