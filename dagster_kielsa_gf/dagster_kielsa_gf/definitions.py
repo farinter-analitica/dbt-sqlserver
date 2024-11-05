f7b7c639f23eab7d81f20a91d5d9ef343a2177a1
Axell <68310020+axellpadilla@users.noreply.github.com>
2024-11-05 02:48:40 -0600

    ajustes

--- dagster_kielsa_gf/dagster_kielsa_gf/definitions.py
+++ dagster_kielsa_gf/dagster_kielsa_gf/definitions.py
@@ -1,52 +1,72 @@
-import os
+from dagster import AssetsDefinition, Definitions
+import dagster_kielsa_gf.dlt.definitions as dlt_defs
+import dagster_kielsa_gf.gobernor.jobs_gobernor as gobernor_defs
 
-from dagster import Definitions, load_assets_from_modules
-from dagster_dbt import DbtCliResource
+from dagster_kielsa_gf import job_control_replicas, jobs
+from dagster_kielsa_gf.assets import (
+    analysis_services,
+    cliente_general,
+    dbt_dwh_kielsa,
+    dbt_example,
+    dbt_sources,
+    examples,
+    knime_asset_factory,
+    ldcom_etl_dwh,
+    ldcom_etl_dwh_sp,
+    recetas_libros_etl_dwh,
+    smb_etl_dwh,
+)
+from dagster_kielsa_gf.schedules import all_schedules
+from dagster_kielsa_gf.sensors import all_sensors
+from dagster_shared_gf import all_shared_resources
 
-from dagster_kielsa_gf.assets import (cliente_general, examples
-                                   , dbt_example
-                                   , dbt_dwh_kielsa
-                                   , dbt_sources
-                                   , smb_etl_dwh
-                                   , knime_asset_factory
-                                   , ldcom_etl_dwh_sp
-                                   , ldcom_etl_dwh
-                                   , recetas_libros_etl_dwh
-                                   , analysis_services)
-
-all_assets = examples.all_assets + dbt_example.all_assets  + dbt_dwh_kielsa.all_assets \
-    + ldcom_etl_dwh_sp.all_assets + knime_asset_factory.all_assets + recetas_libros_etl_dwh.all_assets + analysis_services.all_assets + ldcom_etl_dwh.all_assets \
-    + smb_etl_dwh.all_assets + cliente_general.all_assets
-all_asset_checks = dbt_example.all_asset_checks + dbt_dwh_kielsa.all_asset_checks \
-    + ldcom_etl_dwh_sp.all_asset_checks + knime_asset_factory.all_asset_checks + recetas_libros_etl_dwh.all_asset_checks + analysis_services.all_asset_checks + ldcom_etl_dwh.all_asset_checks \
-    + smb_etl_dwh.all_asset_checks
+all_assets = (
+    *examples.all_assets,
+    *dbt_example.all_assets,
+    *dbt_dwh_kielsa.all_assets,
+    *ldcom_etl_dwh_sp.all_assets,
+    *knime_asset_factory.all_assets,
+    *recetas_libros_etl_dwh.all_assets,
+    *analysis_services.all_assets,
+    *ldcom_etl_dwh.all_assets,
+    *smb_etl_dwh.all_assets,
+    *cliente_general.all_assets,
+)
+all_asset_checks = (
+    *dbt_example.all_asset_checks,
+    *dbt_dwh_kielsa.all_asset_checks,
+    *ldcom_etl_dwh_sp.all_asset_checks,
+    *knime_asset_factory.all_asset_checks,
+    *recetas_libros_etl_dwh.all_asset_checks,
+    *analysis_services.all_asset_checks,
+    *ldcom_etl_dwh.all_asset_checks,
+    *smb_etl_dwh.all_asset_checks,
+)
 
 # Extract the asset keys from the AssetsDefinition instances
 all_asset_keys = set()
 for asset in all_assets:
-    all_asset_keys.update(asset.keys)
+    if type(asset) is AssetsDefinition:
+        all_asset_keys.update(asset.keys)
 
-dbt_sources_assets:list = [source_asset for source_asset in dbt_sources.source_assets if source_asset.key not in all_asset_keys]
+dbt_sources_assets: list = [
+    source_asset
+    for source_asset in dbt_sources.source_assets
+    if source_asset.key not in all_asset_keys
+]
 
-from dagster_kielsa_gf.assets import dbt_dwh_kielsa
-from dagster_shared_gf import all_shared_resources
-from dagster_kielsa_gf import jobs
-from dagster_kielsa_gf import job_control_replicas
-from dagster_kielsa_gf.schedules import all_schedules
-from dagster_kielsa_gf.sensors import all_sensors
 
 all_resources = all_shared_resources
 
 
-import dagster_kielsa_gf.dlt.definitions as dlt_defs
-import dagster_kielsa_gf.gobernor.jobs_gobernor as gobernor_defs
+
 defs = Definitions.merge(
-    #dlt_defs.defs, #antes todos los subrepos
+    # dlt_defs.defs, #antes todos los subrepos
     Definitions(
-        assets=all_assets + dbt_sources_assets + dlt_defs.all_assets,
-        asset_checks=all_asset_checks + dlt_defs.all_asset_checks,
+        assets=(*all_assets, *dbt_sources_assets, *dlt_defs.all_assets,),
+        asset_checks=(*all_asset_checks, *dlt_defs.all_asset_checks),
         resources=all_resources | dlt_defs.all_resources,
-        jobs=jobs.all_jobs + job_control_replicas.all_jobs,
+        jobs=(*jobs.all_jobs, *job_control_replicas.all_jobs),
         schedules=all_schedules,
         sensors=all_sensors,
     ),
--- dagster_kielsa_gf/dagster_kielsa_gf_tests/test_jobs.py
+++ dagster_kielsa_gf/dagster_kielsa_gf_tests/test_jobs.py
@@ -1,12 +1,21 @@
+import warnings
+
+from dagster import (
+    AssetSpec,
+    Definitions,
+    JobDefinition,
+)
+
 from dagster_kielsa_gf import defs
-from dagster import asset, define_asset_job, Definitions, AssetSelection, AssetSpec, JobDefinition
-from dagster_shared_gf.shared_variables import AssetExecutionType, UnresolvedAssetJobDefinition, TagsRepositoryGF as tags_repo
-from typing import Sequence
-from collections import deque
+from dagster_shared_gf.shared_variables import (
+    AssetExecutionType,
+    UnresolvedAssetJobDefinition,
+)
+from dagster_shared_gf.shared_variables import TagsRepositoryGF as tags_repo
 
 defs: Definitions = defs ##import your defs
 asset_spec: AssetSpec 
-
+warnings.filterwarnings("ignore", message="validators are deprecated")
 
 def test_all_assets_assigned_to_a_job():
     # Inspect the Definitions object to find assets without jobs
@@ -15,8 +24,6 @@ def test_all_assets_assigned_to_a_job():
     #all_assets = list(asset_spec.key for asset_spec in defs.get_all_asset_specs())
     for asset_spec in defs.get_all_asset_specs():
         if defs.get_assets_def(asset_spec.key).execution_type == AssetExecutionType.MATERIALIZATION:
-            # if asset_spec.key.path[-1] == 'BI_Kielsa_Dim_ClienteGeneral': 
-            #     print(asset_spec.tags.items())
             all_assets_specs.add(asset_spec.key)
             if asset_spec.automation_condition or tags_repo.DetenerCarga.key in asset_spec.tags:
                 all_auto_stop_assets_specs.add(asset_spec.key)
@@ -27,20 +34,26 @@ def test_all_assets_assigned_to_a_job():
 
     #print(all_assets)
     #print(defs.get_implicit_job_def_for_assets(all_assets))
-
     job_auto_stop_assigned_assets = set()
-    all_jobs:  Sequence[JobDefinition | UnresolvedAssetJobDefinition] = defs.jobs
+    all_jobs = defs.jobs or []
+    all_assets = defs.assets
     for job_def in all_jobs:
-        if hasattr(job_def, 'asset_selection'):
+        if isinstance(job_def, JobDefinition):
             if job_def.asset_selection:
-                for asset_key in job_def.asset_selection:
-                    if defs.get_assets_def(asset_key).execution_type == AssetExecutionType.MATERIALIZATION:
-                        job_auto_stop_assigned_assets.update(asset_key)
-        elif hasattr(job_def, 'selection'):
-            if job_auto_stop_assigned_assets.update(job_def.selection.resolve(defs.assets)):
-                for asset_key in job_auto_stop_assigned_assets.update(job_def.selection.resolve(defs.assets)):
-                    if defs.get_assets_def(asset_key).execution_type == AssetExecutionType.MATERIALIZATION:
-                        job_auto_stop_assigned_assets.update(asset_key)
+                materialized_assets = {
+                    asset_key
+                    for asset_key in job_def.asset_selection
+                    if defs.get_assets_def(asset_key).execution_type == AssetExecutionType.MATERIALIZATION
+                }
+                job_auto_stop_assigned_assets.update(materialized_assets)
+        elif isinstance(job_def, UnresolvedAssetJobDefinition):
+            resolved_assets = job_def.selection.resolve(all_assets) # type: ignore
+            materialized_assets = {
+                asset_key
+                for asset_key in resolved_assets
+                if defs.get_assets_def(asset_key).execution_type == AssetExecutionType.MATERIALIZATION
+            }
+            job_auto_stop_assigned_assets.update(materialized_assets)
 
     job_auto_stop_assigned_assets.update(all_auto_stop_assets_specs)    
     print("Job, auto or stopped assigned assets: ", str(len(job_auto_stop_assigned_assets)))
