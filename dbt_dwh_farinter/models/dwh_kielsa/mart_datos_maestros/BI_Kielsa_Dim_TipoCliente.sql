692abf4515ef3a412fa8a1516e085e89b42a019a
Axell <68310020+axellpadilla@users.noreply.github.com>
2024-12-11 15:03:37 -0600

    tipo cliente

---
+++ dbt_dwh_farinter/models/dwh_kielsa/mart_datos_maestros/BI_Kielsa_Dim_TipoCliente.sql
@@ -0,0 +1,28 @@
+
+{% set unique_key_list = ["TipoCliente_Id","Emp_Id"] %}
+{{ 
+    config(
+		as_columnstore=false,
+		tags=["periodo/diario","periodo/por_hora"],
+		materialized="incremental",
+        incremental_strategy="farinter_merge",
+		unique_key=unique_key_list,
+		on_schema_change="fail",
+		merge_exclude_columns=unique_key_list + ["Fecha_Carga"],
+		merge_check_diff_exclude_columns=unique_key_list + ["Fecha_Carga","Fecha_Actualizado"],
+		post_hook=[
+      "{{ dwh_farinter_remove_incremental_temp_table() }}",
+      "{{ dwh_farinter_create_primary_key(columns=" ~ unique_key_list | tojson ~ ", create_clustered=true, is_incremental=is_incremental(), if_another_exists_drop_it=true) }}",
+      "{{ dwh_farinter_create_dummy_data(unique_key=" ~ unique_key_list | tojson ~ ", is_incremental=0) }}"
+		]
+		
+) }}
+--dbt dagster 
+SELECT --TOP (1000) 
+    [Emp_Id]
+      ,[TipoCliente_Id]
+      ,[TipoCliente_Nombre]
+      ,ISNULL({{ dwh_farinter_hash_column(unique_key_list) }},'') AS [HashStr_TipoCliEmp]
+      ,[Fecha_Carga]
+      ,[Fecha_Actualizado]
+  FROM [DL_FARINTER].[dbo].[DL_Kielsa_Tipo_Cliente] -- {{ source('DL_FARINTER', 'DL_Kielsa_Tipo_Cliente') }}
