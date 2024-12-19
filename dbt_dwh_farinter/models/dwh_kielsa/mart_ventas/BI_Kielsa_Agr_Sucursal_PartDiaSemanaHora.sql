0cb4f494c43d4d38ab04650554ed5dda9954eb49
Axell <68310020+axellpadilla@users.noreply.github.com>
2024-12-19 12:00:15 -0600

    ✨ Agregar nueva consulta para proyección de ventas por sucursal y hora. 🔧 Modificar referencia en la consulta de ventas diarias.

---
+++ dbt_dwh_farinter/models/dwh_kielsa/mart_ventas/BI_Kielsa_Agr_Sucursal_PartDiaSemanaHora.sql
@@ -0,0 +1,87 @@
+{% set unique_key_list = ["Emp_Id","Suc_Id","Dia_Semana_Iso_Id", "Hora_Id"] %}
+{{ 
+    config(
+		as_columnstore=true,
+		tags=["periodo/diario","periodo_unico/si"],
+		materialized="table",
+		incremental_strategy="farinter_merge",
+		unique_key=unique_key_list,
+		merge_exclude_columns=unique_key_list + ["Fecha_Carga"],
+		merge_check_diff_exclude_columns=unique_key_list + ["Fecha_Carga","Fecha_Actualizado"],
+		post_hook=[
+        "{{ dwh_farinter_remove_incremental_temp_table() }}",
+        ]
+	) 
+}}
+{% set v_semanas_muestra = 12 %}
+{% set v_horas_muestra = v_semanas_muestra %}
+{% set v_fecha_inicio = (modules.datetime.datetime.now() - modules.datetime.timedelta(days=v_horas_muestra)).strftime('%Y%m%d') %}
+{% set v_fecha_fin = modules.datetime.datetime.now().strftime('%Y%m%d')  %}
+{% set v_anio_mes_inicio =  v_fecha_inicio[:6]  %}
+
+/*
+--1. Pesos de cada dia de la semana por sucursal, valor y peso
+DECLARE @Inicio AS DATE = GETDATE()
+DECLARE @SemanasPonderacion AS INT = 12
+DECLARE @DiasPonderacion AS INT = @SemanasPonderacion*7 --Historia para ponderar
+DROP TABLE IF EXISTS #Temp
+;*/
+WITH ResumenBase
+AS
+(
+    SELECT 
+        Emp_Id,
+        Suc_Id,
+        FP.Dia_de_la_Semana as Dia_Semana_Iso_Id,
+        FP.Hora_Id as Hora_Id,
+        --@SemanasPonderacion AS Semanas_Muestra,
+        --@HorasPonderacion AS Horas_Muestra,
+        {{ v_horas_muestra }}*1.0 AS Horas_Muestra,
+        ISNULL(SUM(FP.Cantidad_Padre),0)*1.0 AS Sum_Cantidad_Padre,
+        ISNULL(SUM(FP.Valor_Bruto),0)*1.0 AS Sum_Valor_Bruto,
+        ISNULL(SUM(FP.Valor_Neto),0)*1.0 AS Sum_Valor_Neto,
+        ISNULL(SUM(FP.Valor_Costo),0)*1.0 AS Sum_Valor_Costo,
+        ISNULL(SUM(FP.Valor_Descuento),0)*1.0 AS Sum_Valor_Descuento,
+        ISNULL(SUM(FP.Valor_Descuento_Financiero),0)*1.0 AS Sum_Valor_Descuento_Financiero,
+        ISNULL(SUM(FP.Valor_Acum_Monedero),0)*1.0 AS Sum_Valor_Acum_Monedero,
+        ISNULL(SUM(FP.Valor_Descuento_Cupon),0)*1.0 AS Sum_Valor_Descuento_Cupon,
+        ISNULL(SUM(FP.Descuento_Proveedor),0)*1.0 AS Sum_Descuento_Proveedor,
+        ISNULL(SUM(FP.Valor_Descuento_Tercera_Edad),0)*1.0 AS Sum_Valor_Descuento_Tercera_Edad,
+        ISNULL(COUNT(DISTINCT FP.EmpSucDocCajFac_Id),0)*1.0 AS Sum_Conteo_Transacciones
+    FROM BI_FARINTER.dbo.BI_Kielsa_Hecho_FacturaPosicion FP -- {{ ref ('BI_Kielsa_Hecho_FacturaPosicion') }} FP 
+    WHERE Factura_Fecha >= '{{ v_fecha_inicio }}' AND Factura_Fecha < '{{ v_fecha_fin }}' AND AnioMes_Id >= {{ v_anio_mes_inicio }}
+    --WHERE Factura_Fecha >= DATEADD(DAY,- @DiasPonderacion, @Inicio ) AND Factura_Fecha < @inicio
+
+    GROUP BY Emp_Id, Suc_Id, Dia_de_la_Semana, Hora_Id
+)
+SELECT 
+    ISNULL(Emp_Id,0) AS Emp_Id,
+    ISNULL(Suc_Id,0) AS Suc_Id,
+    ISNULL(Dia_Semana_Iso_Id,0) AS Dia_Semana_Iso_Id,
+    ISNULL(Hora_Id,0) AS Hora_Id,
+    {{ dwh_farinter_concat_key_columns(columns=['Emp_Id', 'Suc_Id'], input_length=19, table_alias='')}} [EmpSuc_Id],
+    ISNULL(Horas_Muestra,0) AS Horas_Muestra,
+    CAST(Sum_Cantidad_Padre / Horas_Muestra AS DECIMAL(16,6)) AS Prom_Cantidad_Padre,
+    CAST(Sum_Valor_Bruto / Horas_Muestra AS DECIMAL(16,6)) AS Prom_Valor_Bruto,
+    CAST(Sum_Valor_Neto / Horas_Muestra AS DECIMAL(16,6)) AS Prom_Valor_Neto,
+    CAST(Sum_Valor_Costo / Horas_Muestra AS DECIMAL(16,6)) AS Prom_Valor_Costo,
+    CAST(Sum_Valor_Descuento / Horas_Muestra AS DECIMAL(16,6)) AS Prom_Valor_Descuento,
+    CAST(Sum_Valor_Descuento_Financiero / Horas_Muestra AS DECIMAL(16,6)) AS Prom_Valor_Descuento_Financiero,
+    CAST(Sum_Valor_Acum_Monedero / Horas_Muestra AS DECIMAL(16,6)) AS Prom_Valor_Acum_Monedero,
+    CAST(Sum_Valor_Descuento_Cupon / Horas_Muestra AS DECIMAL(16,6)) AS Prom_Valor_Descuento_Cupon,
+    CAST(Sum_Descuento_Proveedor / Horas_Muestra AS DECIMAL(16,6)) AS Prom_Valor_Descuento_Proveedor,
+    CAST(Sum_Valor_Descuento_Tercera_Edad / Horas_Muestra AS DECIMAL(16,6)) AS Prom_Valor_Descuento_Tercera_Edad,
+    CAST(Sum_Conteo_Transacciones / Horas_Muestra AS DECIMAL(16,6)) AS Prom_Conteo_Transacciones,
+    ISNULL(CAST(Sum_Cantidad_Padre / NULLIF(SUM(Sum_Cantidad_Padre) OVER(PARTITION BY Emp_Id, Suc_Id, Dia_Semana_Iso_Id),0) AS DECIMAL(16,12)),0)  AS Part_Cantidad_Padre,
+    ISNULL(CAST(Sum_Valor_Bruto / NULLIF(SUM(Sum_Valor_Bruto) OVER(PARTITION BY Emp_Id, Suc_Id, Dia_Semana_Iso_Id),0) AS DECIMAL(16,12)),0)  AS Part_Valor_Bruto,
+    ISNULL(CAST(Sum_Valor_Neto / NULLIF(SUM(Sum_Valor_Neto) OVER(PARTITION BY Emp_Id, Suc_Id, Dia_Semana_Iso_Id),0) AS DECIMAL(16,12)),0)  AS Part_Valor_Neto,
+    ISNULL(CAST(Sum_Valor_Costo / NULLIF(SUM(Sum_Valor_Costo) OVER(PARTITION BY Emp_Id, Suc_Id, Dia_Semana_Iso_Id),0) AS DECIMAL(16,12)),0)  AS Part_Valor_Costo,
+    ISNULL(CAST(Sum_Valor_Descuento / NULLIF(SUM(Sum_Valor_Descuento) OVER(PARTITION BY Emp_Id, Suc_Id, Dia_Semana_Iso_Id),0) AS DECIMAL(16,12)),0)  AS Part_Valor_Descuento,
+    ISNULL(CAST(Sum_Valor_Descuento_Financiero / NULLIF(SUM(Sum_Valor_Descuento_Financiero) OVER(PARTITION BY Emp_Id, Suc_Id, Dia_Semana_Iso_Id),0) AS DECIMAL(16,12)),0)  AS Part_Valor_Descuento_Financiero,
+    ISNULL(CAST(Sum_Valor_Acum_Monedero / NULLIF(SUM(Sum_Valor_Acum_Monedero) OVER(PARTITION BY Emp_Id, Suc_Id, Dia_Semana_Iso_Id),0) AS DECIMAL(16,12)),0)  AS Part_Valor_Acum_Monedero,
+    ISNULL(CAST(Sum_Valor_Descuento_Cupon / NULLIF(SUM(Sum_Valor_Descuento_Cupon) OVER(PARTITION BY Emp_Id, Suc_Id, Dia_Semana_Iso_Id),0) AS DECIMAL(16,12)),0)  AS Part_Valor_Descuento_Cupon,
+    ISNULL(CAST(Sum_Descuento_Proveedor / NULLIF(SUM(Sum_Descuento_Proveedor) OVER(PARTITION BY Emp_Id, Suc_Id, Dia_Semana_Iso_Id),0) AS DECIMAL(16,12)),0)  AS Part_Valor_Descuento_Proveedor,
+    ISNULL(CAST(Sum_Valor_Descuento_Tercera_Edad / NULLIF(SUM(Sum_Valor_Descuento_Tercera_Edad) OVER(PARTITION BY Emp_Id, Suc_Id, Dia_Semana_Iso_Id),0) AS DECIMAL(16,12)),0)  AS Part_Valor_Descuento_Tercera_Edad,
+    ISNULL(CAST(Sum_Conteo_Transacciones / NULLIF(SUM(Sum_Conteo_Transacciones) OVER(PARTITION BY Emp_Id, Suc_Id, Dia_Semana_Iso_Id),0) AS DECIMAL(16,12)),0)  AS Part_Conteo_Transacciones
+--INTO #Temp
+FROM ResumenBase
---
+++ dbt_dwh_farinter/models/dwh_kielsa/mart_ventas/BI_Kielsa_Hecho_ProyeccionVenta_SucCanHora.sql
@@ -0,0 +1,80 @@
+{% set unique_key_list = ["Emp_Id","Suc_Id","Fecha_Id","Hora_Id"] %}
+{{ 
+    config(
+		as_columnstore=true,
+		tags=["periodo/diario","periodo_unico/si"],
+		materialized="table",
+		incremental_strategy="farinter_merge",
+		unique_key=unique_key_list,
+		merge_exclude_columns=unique_key_list + ["Fecha_Carga"],
+		merge_check_diff_exclude_columns=unique_key_list + ["Fecha_Carga","Fecha_Actualizado"],
+		post_hook=[
+        "{{ dwh_farinter_remove_incremental_temp_table() }}",
+        "{{ dwh_farinter_create_primary_key(columns=" ~ unique_key_list | tojson ~ ", create_clustered=false, is_incremental=is_incremental(), if_another_exists_drop_it=true) }}",
+        ]
+	) 
+}}
+
+{% set v_fecha_inicio = modules.datetime.datetime.now().strftime('%Y%m%d') %}
+{% set v_fecha_fin = (modules.datetime.datetime.now() + modules.datetime.timedelta(days=60)).strftime('%Y%m%d') %}
+
+/*
+--1. Pesos de cada dia de la semana por sucursal, valor y peso
+DECLARE @Inicio AS DATE = GETDATE()
+DECLARE @SemanasPonderacion AS INT = 12
+DECLARE @DiasPonderacion AS INT = @SemanasPonderacion*7 --Historia para ponderar
+DROP TABLE IF EXISTS #Temp
+;
+*/
+WITH Calculo AS
+(
+    SELECT --TOP (1000) 
+        ISNULL(PR.Emp_Id,0) AS Emp_Id,
+        ISNULL(PR.Suc_Id,0) AS Suc_Id,
+        ISNULL(PDSH.Hora_Id,0) AS Hora_Id,
+        ISNULL(PDS.Dia_Semana_Iso_Id,0) AS Dia_Semana_Iso_Id,
+        ISNULL(CAL.Fecha_Calendario,'1999-01-01') AS [Fecha_Id],
+        CAST((CASE WHEN CAL.Es_Dia_Feriado = 1 THEN PFER.Prom_Cantidad_Padre ELSE PR.Prom_Cantidad_Padre END)
+            *ISNULL(PDS.Part_Cantidad_Padre*7,1)*ISNULL(PDSH.Part_Cantidad_Padre,1)  AS DECIMAL(16,6)) AS Cantidad_Padre,
+        CAST((CASE WHEN CAL.Es_Dia_Feriado = 1 THEN PFER.Prom_Valor_Bruto ELSE PR.Prom_Valor_Bruto END)
+            *ISNULL(PDS.Part_Valor_Bruto*7,1)*ISNULL(PDSH.Part_Valor_Bruto,1)  AS DECIMAL(16,6)) AS Valor_Bruto,
+        CAST((CASE WHEN CAL.Es_Dia_Feriado = 1 THEN PFER.Prom_Valor_Neto ELSE PR.Prom_Valor_Neto END)
+            *ISNULL(PDS.Part_Valor_Neto*7,1)*ISNULL(PDSH.Part_Valor_Neto,1)  AS DECIMAL(16,6)) AS Valor_Neto,
+        CAST((CASE WHEN CAL.Es_Dia_Feriado = 1 THEN PFER.Prom_Valor_Costo ELSE PR.Prom_Valor_Costo END)
+            *ISNULL(PDS.Part_Valor_Costo*7,1)*ISNULL(PDSH.Part_Valor_Costo,1)  AS DECIMAL(16,6)) AS Valor_Costo,
+        CAST((CASE WHEN CAL.Es_Dia_Feriado = 1 THEN PFER.Prom_Valor_Descuento ELSE PR.Prom_Valor_Descuento END)
+            *ISNULL(PDS.Part_Valor_Descuento*7,1)*ISNULL(PDSH.Part_Valor_Descuento,1)  AS DECIMAL(16,6)) AS Valor_Descuento,
+        CAST((CASE WHEN CAL.Es_Dia_Feriado = 1 THEN PFER.Prom_Valor_Descuento_Financiero ELSE PR.Prom_Valor_Descuento_Financiero END)
+            *ISNULL(PDS.Part_Valor_Descuento_Financiero*7,1)*ISNULL(PDSH.Part_Valor_Descuento_Financiero,1)  AS DECIMAL(16,6)) AS Valor_Descuento_Financiero,
+        CAST((CASE WHEN CAL.Es_Dia_Feriado = 1 THEN PFER.Prom_Valor_Acum_Monedero ELSE PR.Prom_Valor_Acum_Monedero END)
+            *ISNULL(PDS.Part_Valor_Acum_Monedero*7,1)*ISNULL(PDSH.Part_Valor_Descuento_Financiero,1)   AS DECIMAL(16,6)) AS Valor_Acum_Monedero,
+        CAST((CASE WHEN CAL.Es_Dia_Feriado = 1 THEN PFER.Prom_Valor_Descuento_Cupon ELSE PR.Prom_Valor_Descuento_Cupon END)
+            *ISNULL(PDS.Part_Valor_Descuento_Cupon*7,1)*ISNULL(PDSH.Part_Valor_Descuento_Cupon,1)  AS DECIMAL(16,6)) AS Valor_Descuento_Cupon,
+        CAST((CASE WHEN CAL.Es_Dia_Feriado = 1 THEN PFER.Prom_Valor_Descuento_Proveedor ELSE PR.Prom_Valor_Descuento_Proveedor END)
+            *ISNULL(PDS.Part_Valor_Descuento_Proveedor*7,1)*ISNULL(PDSH.Part_Valor_Descuento_Proveedor,1)   AS DECIMAL(16,6)) AS Valor_Descuento_Proveedor,
+        CAST((CASE WHEN CAL.Es_Dia_Feriado = 1 THEN PFER.Prom_Valor_Descuento_Tercera_Edad ELSE PR.Prom_Valor_Descuento_Tercera_Edad END)
+            *ISNULL(PDS.Part_Valor_Descuento_Tercera_Edad*7,1)*ISNULL(PDSH.Part_Valor_Descuento_Tercera_Edad,1)    AS DECIMAL(16,6)) AS Valor_Descuento_Tercera_Edad,
+        CAST((CASE WHEN CAL.Es_Dia_Feriado = 1 THEN PFER.Prom_Conteo_Transacciones ELSE PR.Prom_Conteo_Transacciones END)
+            *ISNULL(PDS.Part_Conteo_Transacciones*7,1)*ISNULL(PDSH.Part_Conteo_Transacciones,1)   AS DECIMAL(16,6)) AS Conteo_Transacciones
+
+    FROM {{ ref ('BI_Kielsa_Agr_Sucursal_PromDiaBaseProyec') }} PR
+        INNER JOIN {{ source ('BI_FARINTER', 'BI_Kielsa_Dim_Empresa' ) }} EMP
+            ON EMP.Empresa_Id = PR.Emp_Id
+        INNER JOIN {{ source ('BI_FARINTER', 'BI_Dim_Pais' ) }} PAIS
+            ON PAIS.Pais_Id = EMP.Pais_Id
+        INNER JOIN {{ ref('BI_Dim_Calendario_LaboralPais') }} CAL
+            on CAL.[Fecha_Calendario] >= '{{ v_fecha_inicio }}' AND CAL.[Fecha_Calendario] < '{{ v_fecha_fin }}'
+            AND PAIS.Pais_ISO2 = CAL.Pais_ISO2
+        INNER JOIN {{ ref ('BI_Kielsa_Agr_Sucursal_PartDiaSemana') }} PDS
+            ON PDS.Emp_Id = PR.Emp_Id AND PDS.Suc_Id = PR.Suc_Id
+            AND PDS.Dia_Semana_Iso_Id = CAL.Dia_de_la_Semana
+        INNER JOIN {{ ref ('BI_Kielsa_Agr_Sucursal_PartDiaSemanaHora') }} PDSH
+            ON PDSH.Emp_Id = PR.Emp_Id AND PDSH.Suc_Id = PR.Suc_Id
+            AND PDSH.Dia_Semana_Iso_Id = CAL.Dia_de_la_Semana
+        LEFT JOIN {{ ref ('BI_Kielsa_Agr_Sucursal_PromDiaFeriados') }} PFER
+            ON PFER.Emp_Id = PR.Emp_Id AND PFER.Suc_Id = PR.Suc_Id
+)
+SELECT *,
+    {{ dwh_farinter_concat_key_columns(columns=['Emp_Id', 'Suc_Id'], input_length=19, table_alias='')}} [EmpSuc_Id]
+FROM Calculo
+    
