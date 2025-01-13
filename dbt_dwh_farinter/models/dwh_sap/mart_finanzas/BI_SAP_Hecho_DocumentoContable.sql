868e8470103232b8398acc59eca302f7d6858bbf
Axell <132957665+axellpadilla-farinter@users.noreply.github.com>
2025-01-13 15:01:22 -0600

    :fix: ajustar lógica para determinar la última fecha en modelos de Documento Contable

--- dbt_dwh_farinter/models/dwh_sap/mart_finanzas/BI_SAP_Hecho_DocumentoContable.sql
+++ dbt_dwh_farinter/models/dwh_sap/mart_finanzas/BI_SAP_Hecho_DocumentoContable.sql
@@ -54,8 +54,10 @@
 
 {% if is_incremental() %}
 	{% set last_date = run_single_value_query_on_relation_and_return(query="""select ISNULL(CONVERT(VARCHAR,DATEADD(DAY, -1, max(Fecha_Actualizado)), 112), '20240101')  from  """ ~ this, relation_not_found_value='20240101') %}
+	{% set last_date2 = run_single_value_query_on_relation_and_return(query="""select ISNULL(CONVERT(VARCHAR,DATEADD(DAY, -1, max(Fecha_Contable)), 112), '20240101')  from  """ ~ this, relation_not_found_value='20240101') %}
+    {% set last_date = last_date if last_date < last_date2 else last_date2 %}
 {% else %}
-	{% set last_date = '20240101' %}
+    {% set last_date = '20240101' %}
 {% endif %}
 
 WITH
