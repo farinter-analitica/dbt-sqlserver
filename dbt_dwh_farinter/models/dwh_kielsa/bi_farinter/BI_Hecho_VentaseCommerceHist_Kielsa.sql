a3d717ec7418c42972749da602f854c56e48e084
Axell <132957665+axellpadilla-farinter@users.noreply.github.com>
2025-02-11 21:53:02 -0600

    feat: Add historical ecommerce sales models for Kielsa
    • Create DL_Kielsa_VentaseCommerceHist view to transform MongoDB ecommerce orders data
    • Add BI_Hecho_VentaseCommerceHist_Kielsa view for aggregated sales metrics
    • Configure daily schedule tags and view materialization for both models
    • Add source definitions for MongoDB collections and reference tables
    • Include union with projected ecommerce sales data
    • Implement timezone adjustment and data transformations for sales calculations

---
+++ dbt_dwh_farinter/models/dwh_kielsa/bi_farinter/BI_Hecho_VentaseCommerceHist_Kielsa.sql
@@ -0,0 +1,65 @@
+{% set unique_key_list = [] %}
+
+{{ 
+    config(
+		tags=["periodo/diario"],
+        materialized="view",
+    )
+}}
+--SOLO SE PUEDE CAMBIAR EN DBT
+SELECT A.Pais_Id,
+    A.Sucursal_Id,
+    A.CanalVenta_Id,
+    A.conFactura_Id,
+    A.Origen_Id,
+    A.FormaPago_Id,
+    A.TipoPago_Id,
+    0 AS Tipo_Id,
+    A.Articulo_Id,
+    A.Fecha_Id,
+    year(A.Fecha_Id) AS Anio_Id,
+    MONTH(A.Fecha_Id) AS Mes_Id,
+    DAY(A.Fecha_Id) AS Dia_Id,
+    datepart(weekday, Fecha_Id) AS Dias_Id,
+    sum(A.Cantidad) AS Cantidad,
+    sum(A.Descuento) AS Descuento,
+    sum(A.Venta_Neta) AS Venta_Neta,
+    sum(A.Costo) AS Costo,
+    sum(A.Utilidad) AS Utilidad
+FROM DL_FARINTER.[dbo].[DL_Kielsa_VentaseCommerceHist] AS A -- {{ ref('DL_Kielsa_VentaseCommerceHist') }}
+GROUP BY A.Pais_Id,
+    A.Sucursal_Id,
+    A.CanalVenta_Id,
+    A.conFactura_Id,
+    A.Origen_Id,
+    A.FormaPago_Id,
+    A.TipoPago_Id,
+    A.Articulo_Id,
+    A.Fecha_Id
+UNION ALL
+SELECT A.Pais_Id,
+    A.Principal_Id AS Sucursal_Id,
+    CONVERT(int, A.Categoria1_Id) AS CanalVenta_Id,
+    1 AS conFactura_Id,
+    1 AS Origen_Id,
+    0 AS FormaPago_Id,
+    0 AS TipoPago_Id,
+    2 AS Tipo_Id,
+    A.Categoria2_Id AS Articulo_Id,
+    A.Fecha_Id,
+    year(A.Fecha_Id) AS Anio_Id,
+    MONTH(A.Fecha_Id) AS Mes_Id,
+    DAY(A.Fecha_Id) AS Dia_Id,
+    datepart(weekday, Fecha_Id) AS Dias_Id,
+    sum(A.Cantidad) AS Cantidad,
+    sum(A.Descuento) AS Descuento,
+    sum(A.Venta_Neta) AS Venta_Neta,
+    sum(A.Costo) AS Costo,
+    sum(A.Utilidad) AS Utilidad
+FROM BI_FARINTER.dbo.BI_Hecho_ProyeccionVentaseCommerce_Kielsa AS A -- {{ source('BI_FARINTER', 'BI_Hecho_ProyeccionVentaseCommerce_Kielsa') }}
+WHERE A.Tipo_Id = 2
+GROUP BY A.Pais_Id,
+    A.Principal_Id,
+    A.Categoria1_Id,
+    A.Categoria2_Id,
+    A.Fecha_Id
---
+++ dbt_dwh_farinter/models/dwh_kielsa/dl_farinter/DL_Kielsa_VentaseCommerceHist.sql
@@ -0,0 +1,79 @@
+
+{%- set unique_key_list = [] -%}
+{{ 
+    config(
+		tags=["periodo/diario"],
+		materialized="view",
+) }}
+
+--SOLO SE PUEDE CAMBIAR EN DBT
+
+SELECT A.Emp_Id AS Pais_Id,
+	CASE
+		WHEN B.Factura_Id IS NULL THEN 'x'
+		ELSE '1-' + CONVERT(NVARCHAR, B.Suc_Id) + '-' + CONVERT(NVARCHAR, B.TipoDoc_id) + '-' + CONVERT(NVARCHAR, B.Caja_Id) + '-' + CONVERT(NVARCHAR, B.Factura_Id)
+	END AS Factura_Id,
+	A.Orden_Id,
+	A.OrdenExp_Id,
+	isnull(B.Orden_Usuario_Registro, 'x') AS Usuario_Registro,
+	A.CanalVenta_Id,
+	A.conFactura_Id,
+	isnull(C.Origen_Id, 1) AS Origen_Id,
+	isnull(D.FormaPago_Id, 1) AS FormaPago_Id,
+	isnull(E.TipoPago_Id, 1) AS TipoPago_Id,
+	isnull('1-' + CONVERT(NVARCHAR, B.Suc_Id), 'x') AS Sucursal_Id,
+	'1-' + CONVERT(nvarchar, A.Articulo_Id) AS Articulo_Id,
+	CONVERT(date, A.fecha) AS Fecha_Id,
+	A.Cantidad AS Cantidad,
+	A.Cantidad *(A.Precio_Unitario - isnull(A.Descuento, 0)) AS Venta_Neta,
+	isnull(A.Descuento, 0) AS Descuento,
+	A.Cantidad * A.Costo_Unitario AS Costo,
+	A.Cantidad *(
+		A.Precio_Unitario - isnull(A.Descuento, 0) - A.Costo_Unitario
+	) AS Utilidad
+FROM(
+		SELECT 1 AS Emp_Id,
+			1 AS CC_Id,
+			dateadd(
+				HOUR,
+				-6,
+				try_parse(left(A.created_at, 19) AS datetime)
+			) AS fecha,
+			A._id AS Orden_Id,
+			CASE
+				WHEN A.type_order = 'EX' THEN 2
+				WHEN A.type_order = 'PU' THEN 3
+				ELSE 0
+			END AS CanalVenta_Id,
+			A.origin AS SubCanalVenta_Id,
+			CONVERT(int, isnull(A.Order_number, 0)) AS OrdenExp_Id,
+			A.type_payment AS FormaPago_Id,
+			A.type_pay_card AS TipoPago_Id,
+			CASE
+				WHEN len(A.bill_number_dgi) = 19 THEN 1
+				ELSE 0
+			END AS conFactura_Id,
+			A.[user_id] AS Usuario_Id,
+			isnull(A.bill_number_dgi, 'x') AS Factura_Id,
+			B.other_id_product AS Articulo_Id,
+			B.amount AS Cantidad,
+			B.normal_price AS Precio_Unitario,
+			(B.normal_price - B.price_product) AS Descuento,
+			(B.descuento_tercera_edad / 100.0) * B.price_product AS Descuento_Tercera,
+			(B.porcentaje_isv / 100.0) * B.price_product AS Impuesto,
+			B.costo_producto AS Costo_Unitario
+		FROM DL_FARINTER.mdb_ecommerce_hn.orders AS A -- {{ source('DL_FARINTER_mdb_ecommerce_hn', 'orders') }}
+			INNER JOIN DL_FARINTER.mdb_ecommerce_hn.orders__shopping_cart AS B ON A._dlt_id = B._dlt_parent_id -- {{ source('DL_FARINTER_mdb_ecommerce_hn', 'orders__shopping_cart') }}
+	) AS A
+	LEFT JOIN DL_FARINTER.dbo.DL_Kielsa_Exp_Factura_Express AS B -- {{ source('DL_FARINTER', 'DL_Kielsa_Exp_Factura_Express') }}
+	ON A.Emp_Id = B.Emp_Id
+	AND A.CC_Id = B.CC_Id
+	AND A.OrdenExp_Id = B.Orden_Id
+	LEFT JOIN DL_FARINTER.dbo.DL_Kielsa_eCommerceOrigen AS C -- {{ source('DL_FARINTER', 'DL_Kielsa_eCommerceOrigen') }}
+	ON A.SubCanalVenta_Id = C.Origen_Nombre
+	LEFT JOIN DL_FARINTER.dbo.DL_Kielsa_eCommerceFormaPago AS D -- {{ source('DL_FARINTER', 'DL_Kielsa_eCommerceFormaPago') }}
+	ON A.FormaPago_Id = D.FormaPago_Nombre
+	LEFT JOIN DL_FARINTER.dbo.DL_Kielsa_eCommerceTipoPago AS E  -- {{ source('DL_FARINTER','DL_Kielsa_eCommerceTipoPago') }}
+	ON A.TipoPago_Id = E.TipoPago_Nombre
+WHERE A.Articulo_Id IS NOT NULL
+	AND A.fecha < CONVERT(date, getdate())
