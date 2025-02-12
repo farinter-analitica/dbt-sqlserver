35bd8774a17a0b4787f3ebe3b11638cefafbcc41
Axell <132957665+axellpadilla-farinter@users.noreply.github.com>
2025-02-11 22:44:47 -0600

    fix: [BI_Hecho_VentaseCommerceHist_Kielsa] se alimentaba de 'BI_Hecho_ProyeccionVentaseCommerce_Kielsa que a su vez se alimentaba de 'AN_Param_PesoseCommerce_Kielsa' y a su vez de BI_Hecho_VentaseCommerceHist_Kielsa
    Además no se ingresaron todas las dependencias correctamente (las vistas, que deben existir en dbt para evitar problemas de orden de ejecucion) Lo mismo de resumen
    
    Cambios en [AN_pacargarParam_PesoseCommerce_Kielsa]

---
+++ dbt_dwh_farinter/models/dwh_kielsa/dl_farinter/DL_Hecho_VentaseCommerceHist_Kielsa.sql
@@ -0,0 +1,38 @@
+{% set unique_key_list = [] %}
+
+{{ 
+    config(
+		tags=["periodo/diario"],
+        materialized="view",
+    )
+}}
+--SOLO SE PUEDE CAMBIAR EN DBT
+SELECT A.Pais_Id,
+    A.Sucursal_Id,
+    A.CanalVenta_Id,
+    A.conFactura_Id,
+    A.Origen_Id,
+    A.FormaPago_Id,
+    A.TipoPago_Id,
+    0 AS Tipo_Id,
+    A.Articulo_Id,
+    A.Fecha_Id,
+    year(A.Fecha_Id) AS Anio_Id,
+    MONTH(A.Fecha_Id) AS Mes_Id,
+    DAY(A.Fecha_Id) AS Dia_Id,
+    datepart(weekday, Fecha_Id) AS Dias_Id,
+    sum(A.Cantidad) AS Cantidad,
+    sum(A.Descuento) AS Descuento,
+    sum(A.Venta_Neta) AS Venta_Neta,
+    sum(A.Costo) AS Costo,
+    sum(A.Utilidad) AS Utilidad
+FROM DL_FARINTER.[dbo].[DL_Kielsa_VentaseCommerceHist] AS A -- {{ ref('DL_Kielsa_VentaseCommerceHist') }}
+GROUP BY A.Pais_Id,
+    A.Sucursal_Id,
+    A.CanalVenta_Id,
+    A.conFactura_Id,
+    A.Origen_Id,
+    A.FormaPago_Id,
+    A.TipoPago_Id,
+    A.Articulo_Id,
+    A.Fecha_Id
---
+++ dbt_dwh_farinter/models/dwh_kielsa/dl_farinter/DL_Hecho_VentaseCommerceResumenHist_Kielsa.sql
@@ -0,0 +1,36 @@
+{% set unique_key_list = [] %}
+
+{{ 
+    config(
+		tags=["periodo/diario"],
+        materialized="view",
+    )
+}}
+--SOLO SE PUEDE CAMBIAR EN DBT
+SELECT A.Pais_Id,
+    A.Sucursal_Id,
+    A.CanalVenta_Id,
+    A.FormaPago_Id,
+    A.TipoPago_Id,
+    A.conFactura_Id,
+    A.Origen_Id,
+    0 AS Tipo_Id,
+    A.Fecha_Id,
+    year(A.Fecha_Id) AS Anio_Id,
+    MONTH(A.Fecha_Id) AS Mes_Id,
+    DAY(A.Fecha_Id) AS Dia_Id,
+    datepart(weekday, A.Fecha_Id) AS Dias_Id,
+    COUNT(A.Orden_Id) AS Trx,
+    sum(A.Descuento) AS Descuento,
+    sum(A.Venta_Neta) AS Venta_Neta,
+    sum(A.Costo) AS Costo,
+    sum(A.Utilidad) AS Utilidad
+FROM DL_FARINTER.[dbo].[DL_Kielsa_VentaseCommerceHist] AS A -- {{ ref('DL_Kielsa_VentaseCommerceHist') }}
+GROUP BY A.Pais_Id,
+    A.Sucursal_Id,
+    A.CanalVenta_Id,
+    A.FormaPago_Id,
+    A.conFactura_Id,
+    A.TipoPago_Id,
+    A.Origen_Id,
+    A.Fecha_Id
