500be320fb618ab3b4688f23538d8409cf1f7b42
Axell <132957665+axellpadilla-farinter@users.noreply.github.com>
2025-03-05 14:28:47 -0600

    :sparkles: feat(dwh): agregar dimensión de categoría de tipo de documento
    Se crea la dimensión `BI_SAP_Dim_CategoriaTipoDocumento` en el DWH, que mapea los tipos de documentos SAP a categorías.
    
    - Se crea el modelo dbt para la dimensión.
    - Se agrega la tabla `DD07V` como source en `sources_external.yml` para obtener los datos de la dimensión.
    - Se utiliza la estrategia de merge incremental para la carga de datos.
    - Se definen las claves únicas y las columnas a excluir en el merge.
    - Se incluye post_hook para eliminar tablas temporales y crear la clave primaria.
    - Se agrega datos dummy para la dimensión.

---
+++ dbt_dwh_farinter/models/dwh_sap/bi_farinter/BI_SAP_Dim_CategoriaTipoDocumento.sql
@@ -0,0 +1,30 @@
+{% set unique_key_list = ["CategoriaTipoDocumento_Id"] %}
+{{ 
+    config(
+        as_columnstore=true,
+        tags=["periodo/semanal", "automation/only"],
+        materialized="incremental",
+        incremental_strategy="farinter_merge",
+        unique_key=unique_key_list,
+        merge_exclude_columns=unique_key_list + ["Fecha_Carga"],
+        merge_check_diff_exclude_columns=unique_key_list + ["Fecha_Carga","Fecha_Actualizado"],
+        post_hook=[
+            "{{ dwh_farinter_remove_incremental_temp_table() }}",
+            "{{ dwh_farinter_create_primary_key(columns=" ~ unique_key_list | tojson ~ ", create_clustered=false, is_incremental=is_incremental(), if_another_exists_drop_it=true) }}"
+      		"{{ dwh_farinter_create_dummy_data(unique_key=" ~ unique_key_list | tojson ~ ", is_incremental=0) }}"
+        ]
+    ) 
+}}
+
+SELECT 
+    A.DOMVALUE_L AS CategoriaTipoDocumento_Id,
+    A.DDTEXT AS CategoriaTipoDocumento_Nombre,
+    B.DDTEXT AS CategoriaTipoDocumento_Nombre_EN,
+    GETDATE() AS Fecha_Actualizado
+FROM {{ var('P_SAPPRD_LS') }}.{{ source('SAPPRD','DD07V')}} A
+LEFT JOIN {{ var('P_SAPPRD_LS') }}.{{ source('SAPPRD','DD07V')}} B
+    ON A.DOMVALUE_L = B.DOMVALUE_L 
+    AND B.DOMNAME = 'VBTYP' 
+    AND B.DDLANGUAGE = 'E'
+WHERE A.DOMNAME = 'VBTYP' 
+AND A.DDLANGUAGE = 'S'
