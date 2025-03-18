48c3588219517243686e0cb7c59c17b5ccfd6830
Axell <132957665+axellpadilla-farinter@users.noreply.github.com>
2025-03-18 11:43:03 -0600

    :recycle: refactor(dwh_kielsa): mejorar la dimensión de empleados
    Se refactoriza la consulta de la dimensión de empleados para mejorar la legibilidad y agregar el campo Vendedor_Id y Vendedor_Nombre. Se cambia `on_schema_change` a `append_new_columns`.
    
    Cambios principales:
    - Se agrega el campo `Vendedor_Id` y `Vendedor_Nombre`.
    - Se cambia `on_schema_change` a `append_new_columns`.
    - Se refactoriza la consulta para mejorar la legibilidad.

--- dbt_dwh_farinter/models/dwh_kielsa/mart_datos_maestros/BI_Kielsa_Dim_Empleado.sql
+++ dbt_dwh_farinter/models/dwh_kielsa/mart_datos_maestros/BI_Kielsa_Dim_Empleado.sql
@@ -9,7 +9,7 @@
 		materialized="incremental",
         incremental_strategy="farinter_merge",
 		unique_key=unique_key_list,
-		on_schema_change="fail",
+		on_schema_change="append_new_columns",
 		merge_exclude_columns=unique_key_list + ["Fecha_Carga"],
 		merge_check_diff_exclude_columns=unique_key_list + ["Fecha_Carga","Fecha_Actualizado"],
 		post_hook=[
@@ -28,17 +28,21 @@ SELECT
 	*
 	, {{ dwh_farinter_concat_key_columns(columns=['Emp_Id', 'Empleado_Id'], input_length=29, table_alias='')}} [EmpEmpl_Id]
     , ISNULL({{ dwh_farinter_hash_column(unique_key_list) }},'') AS [HashStr_EmplEmp]
-FROM
-	(SELECT 
-			[Empleado_Id]
-			,[Empleado_Nombre]
-			,[Rol_Id]
-			,[Usuario_Id]
-			,[Rol]
-			,[Emp_Id]
-			,[Hash_EmpleadoEmp]
-			,[Sucursal_Id_Asignado_Meta]
-			,COALESCE([Sucursal_Id_Asignado_Meta],[Sucursal_Id_Ultima_Factura]) AS [Sucursal_Id_Asignado]
-			,[Bit_Activo]
-		FROM {{source ('DL_FARINTER','DL_Kielsa_Empleado')}} 
-  ) A
+FROM (
+		SELECT [Empleado_Id],
+			[Empleado_Nombre],
+			[Empleado_Id] AS [Vendedor_Id],
+			CAST([Empleado_Nombre] AS VARCHAR(50)) AS [Vendedor_Nombre],
+			[Rol_Id],
+			[Usuario_Id],
+			[Rol],
+			[Emp_Id],
+			[Hash_EmpleadoEmp],
+			[Sucursal_Id_Asignado_Meta],
+			COALESCE(
+				[Sucursal_Id_Asignado_Meta],
+				[Sucursal_Id_Ultima_Factura]
+			) AS [Sucursal_Id_Asignado],
+			[Bit_Activo]
+		FROM {{ source ('DL_FARINTER', 'DL_Kielsa_Empleado') }}
+	) A
