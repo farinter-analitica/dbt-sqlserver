d64e0c030fcf0cc3870df6d6e5883b24ddf66000
Axell <132957665+axellpadilla-farinter@users.noreply.github.com>
2025-03-19 22:56:41 -0600

    :sparkles: feat(dwh_kielsa): agregar métricas de monedero en ventana
    Se crean las vistas `BI_Kielsa_Agr_Monedero_Metricas_Ventana`, `BI_Kielsa_Agr_Monedero_DiaSemana_Ventana` y `BI_Kielsa_Agr_Monedero_Ventana` para calcular métricas de monedero en un periodo de ventana.
    
    Cambios principales:
    - Se crean las vistas para calcular métricas como días con compra, promedio de días entre compra y día de la semana preferido.
    - Se utiliza un periodo de ventana de 7 meses para el cálculo de las métricas.
    - Se incluye la lógica para determinar el día de la semana preferido.

---
+++ dbt_dwh_farinter/models/dwh_kielsa/mart_ventas/BI_Kielsa_Agr_Monedero_Ventana.sql
@@ -0,0 +1,65 @@
+
+{% set unique_key_list = ["Monedero_Id","Emp_Id"] %}
+{% set v_merge_exclude_columns = unique_key_list + ["Fecha_Carga"]  %}
+{% set v_merge_check_diff_exclude_columns = v_merge_exclude_columns + ["Fecha_Actualizado"]  %}
+{% set v_last_date = run_single_value_query_on_relation_and_return(query="""select ISNULL(CONVERT(VARCHAR,DATEADD(DAY, 0, max(Fecha_Actualizado)), 112), '19000101')  from  """ ~ this, relation_not_found_value='19000101'|string)|string %}
+{% set v_last_date_control = (modules.datetime.datetime.fromisoformat(v_last_date) - modules.datetime.timedelta(days=30)).strftime('%Y%m%d') %}
+
+{{ 
+    config(
+		as_columnstore=true,
+		tags=["periodo/diario", "periodo_unico/si"],
+		materialized="table",
+		incremental_strategy="farinter_merge",
+		unique_key=unique_key_list,
+		on_schema_change="append_new_columns",
+		merge_exclude_columns= v_merge_exclude_columns,
+		merge_check_diff_exclude_columns= v_merge_check_diff_exclude_columns,
+		post_hook=[
+      "{{ dwh_farinter_create_primary_key(columns=" ~ unique_key_list | tojson ~ ", create_clustered=false, is_incremental=is_incremental(), if_another_exists_drop_it=true) }}",
+		]
+		
+) }}
+
+WITH
+Fecha_Max AS
+(
+	SELECT CAST(MAX(Factura_Fecha) AS DATE) AS Fecha_Max, Emp_Id, MAX(AnioMes_Id) AS AnioMes_Id_Max
+	FROM {{ref("BI_Kielsa_Hecho_FacturaEncabezado")}}
+	WHERE AnioMes_Id >= '{{ v_last_date_control[0:6] }}' and Factura_Fecha >= '{{ v_last_date_control }}'
+	GROUP BY Emp_ID
+),
+Monederos AS
+(
+	--Usamos de base para poder usar diferentes metricas
+	SELECT M.Monedero_Id, M.Emp_Id
+	FROM {{ref("BI_Kielsa_Dim_Monedero")}} M
+)
+	SELECT ISNULL(M.Monedero_Id,'') Monedero_Id     
+		, ISNULL(M.Emp_Id,0) Emp_Id  
+		, ISNULL(MUM.Dias_Con_Compra,0) Dias_Con_Compra
+		, ISNULL(MUM.Meses_Con_Compra,0) AS Meses_Con_Compra
+		, ISNULL(CASE 
+			WHEN MUM.Dias_Con_Compra <= 1 THEN NULL
+			ELSE DATEDIFF(DAY, MUM.Fecha_Primera_Factura, FM.Fecha_Max) / 
+				NULLIF(MUM.Dias_Con_Compra - 1, 0)
+		END, NULL) AS Promedio_Dias_Entre_Compra
+		, ISNULL(MUM.Fecha_Primera_Factura, '19000101') AS Fecha_Primera_Factura
+		, ISNULL(MUM.Fecha_Ultima_Factura, '19000101') AS Fecha_Ultima_Factura
+		, ISNULL(MDS.Dia_Semana_Iso,0) AS Dia_Semana_Iso_Preferido
+		, {{ dwh_farinter_concat_key_columns(columns=['Emp_Id', 'Monedero_Id'], input_length=49, table_alias='M')}} [EmpMon_Id]
+		, GETDATE() AS Fecha_Actualizado
+		, ISNULL(MUM.Ventana_Desde, MDS.Ventana_Desde) AS Ventana_Desde
+	FROM Monederos M
+	LEFT JOIN Fecha_Max FM
+		ON FM.Emp_Id = M.Emp_Id
+	LEFT JOIN {{ ref('BI_Kielsa_Agr_Monedero_Metricas_Ventana') }} MUM
+		ON MUM.Monedero_Id = M.Monedero_Id
+		AND MUM.Emp_Id = M.Emp_Id
+	LEFT JOIN {{ ref('BI_Kielsa_Agr_Monedero_DiaSemana_Ventana') }} MDS
+		ON MDS.Monedero_Id = M.Monedero_Id
+		AND MDS.Emp_Id = M.Emp_Id
+		AND MDS.Ranking = 1
+	WHERE MUM.Monedero_Id IS NOT NULL
+	OR MDS.Monedero_Id IS NOT NULL
+
