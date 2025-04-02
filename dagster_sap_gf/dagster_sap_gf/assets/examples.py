561566ee423095bc5ba4064624ef3bfc60c060f9
Axell <132957665+axellpadilla-farinter@users.noreply.github.com>
2025-04-01 21:33:44 -0600

    :sparkles: feat(sap): agregar assets de ejemplo con tags de automatización
    Se agregan assets de ejemplo para SAP con tags de automatización. Se definen tags para ejecución horaria, diaria, semanal y mensual. Se incluye `automation_condition` para los assets semanales y mensuales, utilizando funciones cron predefinidas.

--- dagster_sap_gf/dagster_sap_gf/assets/examples.py
+++ dagster_sap_gf/dagster_sap_gf/assets/examples.py
@@ -1,23 +1,35 @@
 from dagster import asset
 from dagster_shared_gf.shared_variables import tags_repo
+from dagster_shared_gf.automation import (
+    automation_weekly_1_delta_1_cron,
+    automation_monthly_start_delta_1_cron,
+)
 
 
-@asset(tags=tags_repo.AutomationHourly)
+@asset(tags=tags_repo.AutomationHourly)  # El sensor ya se asigna a los que son por hora
 def sap_example_hourly_automation():
     pass
 
 
-@asset(tags=tags_repo.AutomationDaily)
+@asset(
+    tags=tags_repo.AutomationDaily | tags_repo.Daily
+)  # Un job selecciona los assets que son por dia
 def sap_example_daily_automation():
     pass
 
 
-@asset(tags=tags_repo.AutomationWeekly1)
+@asset(
+    tags=tags_repo.AutomationWeekly1 | tags_repo.Weekly1,
+    automation_condition=automation_weekly_1_delta_1_cron,
+)  # Se asigna ac suponiendo que ningun sensor o job lo asigna
 def sap_example_weekly_automation():
     pass
 
 
-@asset(tags=tags_repo.AutomationMonthlyStart)
+@asset(
+    tags=tags_repo.AutomationMonthlyStart | tags_repo.MonthlyStart,
+    automation_condition=automation_monthly_start_delta_1_cron,
+)  # Se asigna ac suponiendo que ningun sensor o job lo asigna
 def sap_example_monthly_automation():
     pass
 
