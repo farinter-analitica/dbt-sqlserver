ffb1e28104a7a1c675af659e7ef37f363dc59353
AxellPadilla <68310020+axellpadilla@users.noreply.github.com>
2025-04-25 10:49:59 -0600

    :recycle: refactor(dagster): usar load_definitions_from_package_module
    Se refactoriza la carga de definiciones de Dagster para usar `load_definitions_from_package_module` en lugar de `load_definitions_from_modules`. Esto mejora la forma en que se cargan los assets y recursos.

--- dagster_kielsa_gf/dagster_kielsa_gf/dlt_defs/definitions.py
+++ dagster_kielsa_gf/dagster_kielsa_gf/dlt_defs/definitions.py
@@ -1,11 +1,11 @@
 from dagster_dlt import DagsterDltResource
 from dagster_kielsa_gf.dlt_defs import assets
 from dagster_shared_gf.dlt_shared import dlt_resources
-from dagster import load_definitions_from_modules
+from dagster import load_definitions_from_package_module
 
 all_resources = {
     "dlt": DagsterDltResource(),
     "dlt_pipeline_dest_mssql_dwh": dlt_resources.dlt_pipeline_dest_mssql_dwh,
 }
 
-defs = load_definitions_from_modules([assets], resources=all_resources)
+defs = load_definitions_from_package_module(assets, resources=all_resources)
