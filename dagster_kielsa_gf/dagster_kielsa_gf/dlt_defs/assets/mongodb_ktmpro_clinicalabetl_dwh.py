2ceaf63d01a444569d0a2d615f193ca3359eef93
cleymer <cleymer.mendoza@farinter.com>
2025-05-02 10:07:03 -0600

    :sparkles: feat(dwh_kielsa): agregar colección presales a MongoDB
    Se agrega la colección `presales` a la configuración de MongoDB
    para la extracción de datos.
    
    - Se define la configuración para la colección `presales`,
      incluyendo clave primaria, configuración incremental y columnas a
      incluir.
    - Se actualiza la condición de aserción para incluir la nueva
      colección en las pruebas.

--- dagster_kielsa_gf/dagster_kielsa_gf/dlt_defs/assets/mongodb_ktmpro_clinicalabetl_dwh.py
+++ dagster_kielsa_gf/dagster_kielsa_gf/dlt_defs/assets/mongodb_ktmpro_clinicalabetl_dwh.py
@@ -139,6 +139,31 @@ collections_config = (
         ),
         tags=tags_repo.AutomationDaily | tags_repo.AutomationOnly,
     ),
+    DLTRColl(
+        collection_name="presales",
+        primary_key="_id",
+        incrementals=(
+            IncConfig(
+                cursor_path="createdAt",
+                initial_value=get_for_current_env(
+                    {
+                        "local": pendulum.now().subtract(days=60),
+                        "dev": pendulum.now().subtract(years=4),
+                    }
+                ),
+                lag=7,  # days
+            ),
+        ),
+        automation_condition=automation_daily_delta_2_cron,
+        columns_to_include=(
+            "_id",
+            "arrayProducts",
+            "arrayServices",
+            "createdAt",
+            "idVideo",
+        ),
+        tags=tags_repo.AutomationDaily | tags_repo.AutomationOnly,
+    ),
 )
 
 mongodb_ktmpro_clinicalab = mongodb(
@@ -203,9 +228,7 @@ if __name__ == "__main__":
                 if asset.key
                 in (
                     AssetKey(("dlt_mongodb",)),
-                    AssetKey(
-                        ("DL_FARINTER", "mdb_ktmpro_clinicalab", "consultdoctoroffices")
-                    ),
+                    AssetKey(("DL_FARINTER", "mdb_ktmpro_clinicalab", "presales")),
                 )
             )
             assert asset_to_test
