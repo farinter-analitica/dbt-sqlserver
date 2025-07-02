04e2e9a51b2e2e80344e8c9129ef1538be7d4223
AxellPadilla <68310020+axellpadilla@users.noreply.github.com>
2025-07-02 17:24:57 +0000

    :recycle: refactor(dwh_kielsa): mejorar lógica de validación de transacciones KPP
    - Se refactoriza la lógica para determinar si una transacción es válida en
      `DL_Kielsa_KPP_Transacciones_Validas.sql`.
    - Se agrega `ISNULL` para manejar valores nulos en `ResponseSuccess`.
    - Se utilizan alias para mejorar la legibilidad de las consultas.

--- dbt_dwh_farinter/models/dwh_kielsa/dl_farinter/DL_Kielsa_KPP_Transacciones_Validas.sql
+++ dbt_dwh_farinter/models/dwh_kielsa/dl_farinter/DL_Kielsa_KPP_Transacciones_Validas.sql
@@ -20,31 +20,34 @@
 ) }}
 
 {% if is_incremental() %}
-	{% set last_date = run_single_value_query_on_relation_and_return(query="""select ISNULL(CONVERT(VARCHAR,DATEADD(DAY, -7, max(Fecha_Actualizado)), 112), '19000101')  from  """ ~ this, relation_not_found_value='00000000'|string)|string %}
+    {% set last_date = run_single_value_query_on_relation_and_return(
+        query="""select ISNULL(CONVERT(VARCHAR,DATEADD(DAY, -7, max(Fecha_Actualizado)), 112), '19000101')  from  """ ~ this,
+        relation_not_found_value='00000000'|string)|string %}
 {% else %}
-	{% set last_date = '00000000'|string %}
+    {% set last_date = '00000000'|string %}
 {% endif %}
 
 WITH TransactionSequence AS (
-    SELECT t.[Transaccion_Id],
-        t.[TarjetaKC_Id],
-        t.[Cliente_Nombre],
-        t.[Monto_Rebajado],
-        t.[Fecha],
-        t.[NumeroTarjeta],
-        t.[TipoTarjeta],
-        t.[ResponseMessage],
-        t.[transaction_id],
-        t.[transaction_reference],
-        t.[payment_uuid],
-        t.[payment_hash],
-        t.[response_reason],
-        t.[ResponseSuccess],
-		t.Fecha_Actualizado,
+    SELECT
+        t.Transaccion_Id,
+        t.TarjetaKC_Id,
+        t.Cliente_Nombre,
+        t.Monto_Rebajado,
+        t.Fecha,
+        t.NumeroTarjeta,
+        t.TipoTarjeta,
+        t.ResponseMessage,
+        t.transaction_id,
+        t.transaction_reference,
+        t.payment_uuid,
+        t.payment_hash,
+        t.response_reason,
+        t.ResponseSuccess,
+        t.Fecha_Actualizado,
         -- Contamos transacciones positivas anteriores en el mismo día
         COUNT(
             CASE
-                WHEN Monto_Rebajado > 0 THEN 1
+                WHEN t.Monto_Rebajado > 0 THEN 1
             END
         ) OVER (
             PARTITION BY t.TarjetaKC_Id,
@@ -55,48 +58,59 @@ WITH TransactionSequence AS (
             PARTITION BY t.TarjetaKC_Id
             ORDER BY t.Transaccion_Id
         ) AS Siguiente_Monto
-    FROM {{ ref('DL_Kielsa_KPP_Transacciones') }} t 
-	{% if is_incremental() %}
-    WHERE Fecha_Actualizado > '{{ last_date }}' 
-	{% endif %}
+    FROM {{ ref('DL_Kielsa_KPP_Transacciones') }} AS t
+    {% if is_incremental() %}
+        WHERE t.Fecha_Actualizado > '{{ last_date }}'
+    {% endif %}
 ),
+
 LogValidation AS (
-    SELECT TarjetaKC_Id,
+    SELECT
+        TarjetaKC_Id,
         CAST(FRegistro AS DATE) AS Fecha_Log,
         COUNT(*) AS Total_Errores,
         STRING_AGG(ErrorMessage, ' | ') WITHIN GROUP (ORDER BY FRegistro) AS Errores_Concatenados
     FROM {{ ref('DL_Kielsa_KPP_LogSuscripcion') }}
-    WHERE (ErrorMessage LIKE '%error%' and ErrorMessage NOT LIKE '%usuario no encontrado%')
+    WHERE
+        (ErrorMessage LIKE '%error%' AND ErrorMessage NOT LIKE '%usuario no encontrado%')
         OR ErrorMessage LIKE '%unauth%'
         OR ErrorMessage LIKE '%time%out%'
-    GROUP BY TarjetaKC_Id,
+    GROUP BY
+        TarjetaKC_Id,
         CAST(FRegistro AS DATE)
 ),
+
 ValidTransactions AS (
-    SELECT t.*,
+    SELECT
+        t.*,
         l.Errores_Concatenados AS Mensajes_Error,
         l.Total_Errores,
         CASE
-            WHEN t.Monto_Rebajado > 0
-            AND (
-                Siguiente_Monto IS NULL
-                OR Siguiente_Monto >= 0
-            ) -- Si hay errores, verificamos que sean menos que las transacciones positivas previas
-            AND (
-                l.Total_Errores IS NULL
-                OR t.ResponseSuccess LIKE '%true%'
-                OR l.Total_Errores <= t.Transacciones_Positivas_Previas
-            )
-            AND t.ResponseSuccess NOT LIKE '%false%' THEN 1
-            WHEN t.Monto_Rebajado > 0
-            AND Siguiente_Monto = - t.Monto_Rebajado THEN 0
+            WHEN
+                t.Monto_Rebajado > 0
+                AND (
+                    t.Siguiente_Monto IS NULL
+                    OR t.Siguiente_Monto >= 0
+                ) -- Si hay errores, verificamos que sean menos que las transacciones positivas previas
+                AND (
+                    l.Total_Errores IS NULL
+                    OR ISNULL(t.ResponseSuccess, '') LIKE '%true%'
+                    OR l.Total_Errores <= t.Transacciones_Positivas_Previas
+                )
+                AND ISNULL(t.ResponseSuccess, '') NOT LIKE '%false%' THEN 1
+            WHEN
+                t.Monto_Rebajado > 0
+                AND t.Siguiente_Monto = -t.Monto_Rebajado THEN 0
             ELSE 0
         END AS Es_Transaccion_Valida
-    FROM TransactionSequence t
-        LEFT JOIN LogValidation l ON t.TarjetaKC_Id = l.TarjetaKC_Id
-        AND CAST(t.Fecha AS DATE) = l.Fecha_Log
+    FROM TransactionSequence AS t
+    LEFT JOIN LogValidation
+        AS l ON t.TarjetaKC_Id = l.TarjetaKC_Id
+    AND CAST(t.Fecha AS DATE) = l.Fecha_Log
 )
-SELECT *,
+
+SELECT
+    *,
     CASE WHEN Es_Transaccion_Valida = 1 AND Total_Errores > 0 THEN 1 ELSE 0 END AS Es_Transaccion_Sospechosa
 FROM ValidTransactions
 --WHERE TarjetaKC_Id = '0603-1989-00447' --ORDER BY Fecha;
