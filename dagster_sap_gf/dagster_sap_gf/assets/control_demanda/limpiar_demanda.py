2f233d6245722d8dd8cf621f8f7293d1ac138740
AxellPadilla <68310020+axellpadilla@users.noreply.github.com>
2025-07-04 04:26:05 +0000

    :bug: fix(etl): bug fix de libreria y migrar ops a decoradores de Dagster
    - Se actualizan las definiciones de `op` a decoradores `@dg.op` de Dagster para mayor consistencia.
    - Se agrega un `op` para medir el tiempo de ejecución del job.
    - Se agrega metadatos al contexto de ejecución para registrar la duración del job.

--- dagster_sap_gf/dagster_sap_gf/assets/control_demanda/limpiar_demanda.py
+++ dagster_sap_gf/dagster_sap_gf/assets/control_demanda/limpiar_demanda.py
@@ -4,20 +4,7 @@ from datetime import datetime
 from dagster_shared_gf.shared_helpers import DataframeSQLScriptGenerator
 import pendulum as pdl
 import polars as pl
-from dagster import (
-    AssetKey,
-    AssetsDefinition,
-    Field,
-    In,
-    Nothing,
-    OpExecutionContext,
-    Out,
-    asset,
-    graph,
-    instance_for_test,
-    materialize,
-    op,
-)
+import dagster as dg
 
 from dagster_shared_gf.automation import automation_monthly_start_delta_1_cron
 from dagster_shared_gf.resources.smb_resources import (
@@ -29,21 +16,21 @@ from dagster_shared_gf.resources.sql_server_resources import (
 from dagster_shared_gf.shared_variables import env_str, tags_repo
 
 
-@op(
+@dg.op(
     ins={
-        "BI_SAP_Agr_SocAlmArt_Stock_Plan": In(
-            dagster_type=Nothing,
+        "BI_SAP_Agr_SocAlmArt_Stock_Plan": dg.In(
+            dagster_type=dg.Nothing,
         ),
     },
     out={
-        "df_stock": Out(pl.DataFrame, io_manager_key="polars_parquet_io_manager"),
+        "df_stock": dg.Out(pl.DataFrame, io_manager_key="polars_parquet_io_manager"),
     },
     config_schema={
-        "meses_muestra": Field(int, is_required=False, default_value=12 * 5)
+        "meses_muestra": dg.Field(int, is_required=False, default_value=12 * 5)
     },
 )
 def get_BI_SAP_Agr_SocAlmArt_Stock_Plan_data(
-    context: OpExecutionContext,
+    context: dg.OpExecutionContext,
     dwh_farinter_bi: SQLServerResource,
 ) -> pl.DataFrame:
     meses_muestra = context.op_config["meses_muestra"]
@@ -67,7 +54,6 @@ def get_BI_SAP_Agr_SocAlmArt_Stock_Plan_data(
         ,[Articulo_Id]
     FROM [BI_FARINTER].[dbo].[BI_SAP_Agr_SocAlmArt_Stock_Plan]
     WHERE Fecha_Id >= '{fecha_desde.strftime("%Y%m%d")}'
-    --AND Gpo_Obs_Nombre_Corto IN ('Farma_Imp_Cod','Farma_Imp_Exc','Cons_Imp_Exc')        
     """
     main_query = (
         pl.read_database(sql_query, dwh_farinter_bi.get_arrow_odbc_conn_string())
@@ -78,21 +64,21 @@ def get_BI_SAP_Agr_SocAlmArt_Stock_Plan_data(
     return main_query
 
 
-@op(
+@dg.op(
     ins={
-        "BI_SAP_Agr_SocAlmArtGpoCli_Demanda_Plan": In(
-            dagster_type=Nothing,
+        "BI_SAP_Agr_SocAlmArtGpoCli_Demanda_Plan": dg.In(
+            dagster_type=dg.Nothing,
         ),
     },
     out={
-        "df_demanda": Out(pl.DataFrame, io_manager_key="polars_parquet_io_manager"),
+        "df_demanda": dg.Out(pl.DataFrame, io_manager_key="polars_parquet_io_manager"),
     },
     config_schema={
-        "meses_muestra": Field(int, is_required=False, default_value=12 * 5)
+        "meses_muestra": dg.Field(int, is_required=False, default_value=12 * 5)
     },
 )
 def get_BI_SAP_Agr_SocAlmArtGpoCli_Demanda_Plan_data(
-    context: OpExecutionContext,
+    context: dg.OpExecutionContext,
     dwh_farinter_bi: SQLServerResource,
 ) -> pl.DataFrame:
     meses_muestra = context.op_config["meses_muestra"]
@@ -118,7 +104,6 @@ def get_BI_SAP_Agr_SocAlmArtGpoCli_Demanda_Plan_data(
         ,[Articulo_Id]
     FROM [BI_FARINTER].[dbo].[BI_SAP_Agr_SocAlmArtGpoCli_Demanda_Plan]
     WHERE Fecha_Id >= '{fecha_desde.strftime("%Y%m%d")}'
-    --AND Gpo_Obs_Nombre_Corto IN ('Farma_Imp_Cod','Farma_Imp_Exc','Cons_Imp_Exc')        
     """
     main_query = (
         pl.read_database(sql_query, dwh_farinter_bi.get_arrow_odbc_conn_string())
@@ -129,9 +114,9 @@ def get_BI_SAP_Agr_SocAlmArtGpoCli_Demanda_Plan_data(
     return main_query
 
 
-@op(
+@dg.op(
     out={
-        "df_demanda_procesada": Out(
+        "df_demanda_procesada": dg.Out(
             pl.DataFrame, io_manager_key="polars_parquet_io_manager"
         ),
     },
@@ -153,9 +138,12 @@ def procesar_con_mddme_op(
     return procesar_con_mddme(df_demanda, df_stock, procesar_stats=True, umbral_min=0.4)
 
 
-@op
+@dg.op
 def save_demanda_procesada(
-    dwh_farinter_bi: SQLServerResource, demanda_procesada: pl.DataFrame
+    context: dg.OpExecutionContext,
+    dwh_farinter_bi: SQLServerResource,
+    demanda_procesada: pl.DataFrame,
+    start_time: float,
 ) -> None:
     if env_str == "local":
         with pl.Config() as c:
@@ -163,70 +151,89 @@ def save_demanda_procesada(
             c.set_tbl_cols(-1)
             print(demanda_procesada.head(10))
             print(demanda_procesada.describe())
-        return
-    print(f"Por guardar {len(demanda_procesada)} registros")
-    with dwh_farinter_bi.get_sqlalchemy_conn() as conn:
-        sg = DataframeSQLScriptGenerator(
-            primary_keys=(
-                "Material_Id",
-                "Fecha_Id",
-                "Centro_Almacen_Id",
-                "Gpo_Cliente",
-            ),
-            db_schema="dbo",
-            table_name="BI_SAP_Hecho_SocAlmArtGpoCli_Demanda_Limpia",
-            df=demanda_procesada,
-            temp_table_name="BI_SAP_Hecho_SocAlmArtGpoCli_Demanda_Limpia_NEW",
-        )
+    else:
+        print(f"Por guardar {len(demanda_procesada)} registros")
+        with dwh_farinter_bi.get_sqlalchemy_conn() as conn:
+            sg = DataframeSQLScriptGenerator(
+                primary_keys=(
+                    "Material_Id",
+                    "Fecha_Id",
+                    "Centro_Almacen_Id",
+                    "Gpo_Cliente",
+                ),
+                db_schema="dbo",
+                table_name="BI_SAP_Hecho_SocAlmArtGpoCli_Demanda_Limpia",
+                df=demanda_procesada,
+                temp_table_name="BI_SAP_Hecho_SocAlmArtGpoCli_Demanda_Limpia_NEW",
+            )
 
-        dwh_farinter_bi.execute_and_commit(
-            sg.drop_table_sql_script(temp=True), connection=conn
-        )
-        dwh_farinter_bi.execute_and_commit(
-            sg.create_table_sql_script(temp=True), connection=conn
-        )
-        dwh_farinter_bi.execute_and_commit(
-            sg.columnstore_table_sql_script(temp=True), connection=conn
-        )
+            dwh_farinter_bi.execute_and_commit(
+                sg.drop_table_sql_script(temp=True), connection=conn
+            )
+            dwh_farinter_bi.execute_and_commit(
+                sg.create_table_sql_script(temp=True), connection=conn
+            )
+            dwh_farinter_bi.execute_and_commit(
+                sg.columnstore_table_sql_script(temp=True), connection=conn
+            )
 
-        # First write as regular table
-        demanda_procesada.write_database(
-            table_name=sg.temp_table_name,
-            connection=conn,
-            if_table_exists="append",
-        )
+            # First write as regular table
+            demanda_procesada.write_database(
+                table_name=sg.temp_table_name,
+                connection=conn,
+                if_table_exists="append",
+            )
 
-        dwh_farinter_bi.execute_and_commit(
-            sg.primary_key_table_sql_script(temp=True), connection=conn
-        )
+            dwh_farinter_bi.execute_and_commit(
+                sg.primary_key_table_sql_script(temp=True), connection=conn
+            )
 
-        dwh_farinter_bi.execute_and_commit(sg.swap_table_with_temp(), connection=conn)
+            dwh_farinter_bi.execute_and_commit(
+                sg.swap_table_with_temp(), connection=conn
+            )
+
+    context.add_output_metadata(
+        metadata={
+            "rows": len(demanda_procesada),
+            "start_time": start_time,
+            "end_time": datetime.now().timestamp(),
+            "duration_seconds": datetime.now().timestamp() - start_time,
+        }
+    )
+
+    return
 
 
-@graph
+@dg.op
+def start_timer_op() -> float:
+    return datetime.now().timestamp()
+
+
+@dg.graph
 def limpiar_demanda_graph(
     BI_SAP_Agr_SocAlmArtGpoCli_Demanda_Plan, BI_SAP_Agr_SocAlmArt_Stock_Plan
 ):
+    start_time = start_timer_op()
     df_demanda = get_BI_SAP_Agr_SocAlmArtGpoCli_Demanda_Plan_data(
         BI_SAP_Agr_SocAlmArtGpoCli_Demanda_Plan
     )
     df_stock = get_BI_SAP_Agr_SocAlmArt_Stock_Plan_data(BI_SAP_Agr_SocAlmArt_Stock_Plan)
     demanda_procesada_mddme = procesar_con_mddme_op(df_demanda, df_stock)
-    return save_demanda_procesada(demanda_procesada_mddme)
+    return save_demanda_procesada(demanda_procesada_mddme, start_time)
 
 
-BI_SAP_Hecho_SocAlmArtGpoCli_Demanda_Limpia = AssetsDefinition.from_graph(
+BI_SAP_Hecho_SocAlmArtGpoCli_Demanda_Limpia = dg.AssetsDefinition.from_graph(
     graph_def=limpiar_demanda_graph,
     keys_by_input_name={
-        "BI_SAP_Agr_SocAlmArtGpoCli_Demanda_Plan": AssetKey(
+        "BI_SAP_Agr_SocAlmArtGpoCli_Demanda_Plan": dg.AssetKey(
             ["BI_FARINTER", "dbo", "BI_SAP_Agr_SocAlmArtGpoCli_Demanda_Plan"]
         ),
-        "BI_SAP_Agr_SocAlmArt_Stock_Plan": AssetKey(
+        "BI_SAP_Agr_SocAlmArt_Stock_Plan": dg.AssetKey(
             ["BI_FARINTER", "dbo", "BI_SAP_Agr_SocAlmArt_Stock_Plan"]
         ),
     },
     keys_by_output_name={
-        "result": AssetKey(
+        "result": dg.AssetKey(
             ["BI_FARINTER", "dbo", "BI_SAP_Hecho_SocAlmArtGpoCli_Demanda_Limpia"]
         )
     },
@@ -261,14 +268,14 @@ if __name__ == "__main__":
                 "Running in local mode, using top 10000 rows and no loading to SQL Server"
             )
 
-        @asset(name="between_asset")
+        @dg.asset(name="between_asset")
         def mock_between_asset() -> int:
             return 1
 
         mock_dwh_farinter_bi = ResourceDefinition.mock_resource()
         mock_dwh_farinter_dl = ResourceDefinition.mock_resource()
 
-        result = materialize(
+        result = dg.materialize(
             assets=[mock_between_asset, BI_SAP_Hecho_SocAlmArtGpoCli_Demanda_Limpia],
             instance=instance,
             resources={
