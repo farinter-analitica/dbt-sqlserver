38ac8b1a5c1d874d3c039949359c1fa5a1f47efd
AxellPadilla <68310020+axellpadilla@users.noreply.github.com>
2025-07-24 22:35:18 +0000

    :recycle: refactor(bi_farinter): mejorar configuración de modelo de pago

--- dbt_dwh_farinter/models/dwh_kielsa/bi_farinter/BI_Kielsa_Dim_Forma_Pago.sql
+++ dbt_dwh_farinter/models/dwh_kielsa/bi_farinter/BI_Kielsa_Dim_Forma_Pago.sql
@@ -16,8 +16,13 @@
     config(
         as_columnstore=true,
         tags=["automation/eager"],
-        materialized="incremental",
-        post_hook=[
+		materialized="incremental",
+		incremental_strategy="farinter_merge",
+		unique_key=unique_key_list,
+		on_schema_change="append_new_columns",
+		merge_exclude_columns=unique_key_list + ["Fecha_Carga"],
+		merge_check_diff_exclude_columns=unique_key_list + ["Fecha_Carga","Fecha_Actualizado"],
+		post_hook=[
         "{{ dwh_farinter_create_primary_key(columns=" ~ unique_key_list | tojson ~ ", create_clustered=false, is_incremental=false, if_another_exists_drop_it=true) }}"
         ]
     ) 
