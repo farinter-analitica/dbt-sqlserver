78e0636153b8d940e7330427aa33bed7080a3f11
AxellPadilla <68310020+axellpadilla@users.noreply.github.com>
2025-09-04 20:33:14 +0000

    :recycle: refactor(dwh_kielsa): mejorar formato y estructura de consulta SQL

--- dbt_dwh_farinter/models/dwh_kielsa/dl_farinter/DL_TC_ArticuloXMecanica_Kielsa.sql
+++ dbt_dwh_farinter/models/dwh_kielsa/dl_farinter/DL_TC_ArticuloXMecanica_Kielsa.sql
@@ -17,25 +17,29 @@
 }}
 
 SELECT
-	ISNULL(CONCAT(A.Emp_Id, '-', B.Alerta_Id),'X') AS MecanicaCanje_Id
-	, ISNULL(A.Articulo_Id,'') AS Articulo_Id
-	, ISNULL(A.Alerta_Id,0) AS Alerta_Id
-	, isnull(A.Emp_Id,0) AS Emp_Id
-	, B.Fecha_Inicio AS Inicio
-	, B.Fecha_Final AS Final
-	, B.Activa AS Estado
-	, GETDATE() AS Fecha_Actualizado
+    B.Fecha_Inicio AS Inicio,
+    B.Fecha_Final AS Final,
+    B.Activa AS Estado,
+    ISNULL(CONCAT(A.Emp_Id, '-', B.Alerta_Id), 'X') AS MecanicaCanje_Id,
+    ISNULL(A.Articulo_Id, '') AS Articulo_Id,
+    ISNULL(A.Alerta_Id, 0) AS Alerta_Id,
+    ISNULL(A.Emp_Id, 0) AS Emp_Id,
+    GETDATE() AS Fecha_Actualizado
 FROM
-	(SELECT
-		MAX(B.Alerta_Id) AS Alerta_Id, A.Emp_Id, A.Articulo_Id, MAX(CAST(B.Activa AS INT)) AS activa
-	FROM	DL_FARINTER.dbo.DL_Kielsa_Articulo_Alerta A --{{ source ('DL_FARINTER', 'DL_Kielsa_Articulo_Alerta') }}
-	INNER JOIN DL_FARINTER.dbo.DL_Kielsa_PV_Alerta B --{{ source ('DL_FARINTER', 'DL_Kielsa_PV_Alerta') }}
-		ON A.Alerta_Id = B.Alerta_Id AND A.Emp_Id = B.Emp_Id
-	WHERE (SUBSTRING(B.Nombre, 1, 5) = 'CANJE' or B.Nombre like '%CANJE%') --AND A.Emp_Id=1
-											-- and B.Alerta_Id <> 4
-		AND B.Activa = 1
-	GROUP BY A.Articulo_Id, A.Emp_Id) A
-INNER JOIN DL_FARINTER.dbo.DL_Kielsa_PV_Alerta B --{{ source ('DL_FARINTER', 'DL_Kielsa_PV_Alerta') }}
-	ON A.Alerta_Id = B.Alerta_Id AND A.Emp_Id = B.Emp_Id
-
-	
+    (
+        SELECT
+            A.Emp_Id,
+            A.Articulo_Id,
+            B.Alerta_Id AS [Alerta_Id],
+            B.Activa AS [activa],
+            ROW_NUMBER() OVER (PARTITION BY A.Articulo_Id, A.Emp_Id ORDER BY B.Fecha_Final DESC) AS rn
+        FROM {{ source ('DL_FARINTER', 'DL_Kielsa_Articulo_Alerta') }} AS A
+        INNER JOIN {{ source ('DL_FARINTER', 'DL_Kielsa_PV_Alerta') }} AS B
+            ON A.Alerta_Id = B.Alerta_Id AND A.Emp_Id = B.Emp_Id
+        WHERE (SUBSTRING(B.Nombre, 1, 5) = 'CANJE' OR B.Nombre LIKE '%CANJE%') --AND A.Emp_Id=1
+        -- and B.Alerta_Id <> 4
+        AND B.Activa = 1
+    ) AS A
+INNER JOIN {{ source ('DL_FARINTER', 'DL_Kielsa_PV_Alerta') }} AS B
+    ON A.Alerta_Id = B.Alerta_Id AND A.Emp_Id = B.Emp_Id
+WHERE A.rn = 1
