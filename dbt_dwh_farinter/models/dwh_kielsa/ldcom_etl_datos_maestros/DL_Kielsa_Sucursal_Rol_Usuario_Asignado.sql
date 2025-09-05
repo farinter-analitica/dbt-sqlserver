2bffaa1e728107ee3d7f9cbe197c5f71fb24dd03
AxellPadilla <68310020+axellpadilla@users.noreply.github.com>
2025-09-05 18:21:14 +0000

    :recycle: refactor(dwh_kielsa): mejorar la estructura de la consulta SQL

--- dbt_dwh_farinter/models/dwh_kielsa/ldcom_etl_datos_maestros/DL_Kielsa_Sucursal_Rol_Usuario_Asignado.sql
+++ dbt_dwh_farinter/models/dwh_kielsa/ldcom_etl_datos_maestros/DL_Kielsa_Sucursal_Rol_Usuario_Asignado.sql
@@ -34,143 +34,174 @@ WHERE LS_LDCOM_RepLocal IS NOT NULL and Es_Empresa_Principal = 1
 {%- endif %} #}
 
 WITH rol_supervisor AS (
-	SELECT *,
-		(
-			CASE
-				WHEN Emp_Id = 5
-				AND Rol_Nombre IN ('Supervisor de Zona','Supervisor de Operaciones') THEN 'Supervisor'
-				WHEN Emp_Id = 3
-				AND Rol_Nombre LIKE 'Supervisor de Operaciones' THEN 'Supervisor'
-				WHEN Rol_Nombre LIKE 'Supervisor de Operaciones' THEN 'Supervisor'
-				ELSE ''
-			END
-		) AS Rol_Sucursal
-	FROM DL_FARINTER.DBO.DL_Kielsa_Seg_Rol -- {{ source('DL_FARINTER', 'DL_Kielsa_Seg_Rol') }}
-	WHERE (
-			CASE
-				WHEN Emp_Id = 5
-				AND Rol_Nombre LIKE 'Supervisor de Zona' THEN 'Supervisor'
-				WHEN Emp_Id = 3
-				AND Rol_Nombre LIKE 'Supervisor de Operaciones' THEN 'Supervisor'
-				WHEN Rol_Nombre LIKE 'Supervisor de Operaciones' THEN 'Supervisor'
-				ELSE ''
-			END
-		) = 'Supervisor'
+    SELECT
+        *,
+        (
+            CASE
+                WHEN
+                    Emp_Id = 5
+                    AND Rol_Nombre IN ('Supervisor de Zona', 'Supervisor de Operaciones') THEN 'Supervisor'
+                WHEN
+                    Emp_Id = 3
+                    AND Rol_Nombre LIKE 'Supervisor de Operaciones' THEN 'Supervisor'
+                WHEN Rol_Nombre LIKE 'Supervisor de Operaciones' THEN 'Supervisor'
+                ELSE ''
+            END
+        ) AS Rol_Sucursal
+    FROM DL_FARINTER.DBO.DL_Kielsa_Seg_Rol -- {{ source('DL_FARINTER', 'DL_Kielsa_Seg_Rol') }}
+    WHERE (
+        CASE
+            WHEN
+                Emp_Id = 5
+                AND Rol_Nombre LIKE 'Supervisor de Zona' THEN 'Supervisor'
+            WHEN
+                Emp_Id = 3
+                AND Rol_Nombre LIKE 'Supervisor de Operaciones' THEN 'Supervisor'
+            WHEN Rol_Nombre LIKE 'Supervisor de Operaciones' THEN 'Supervisor'
+            ELSE ''
+        END
+    ) = 'Supervisor'
 ),
+
 rol_jop AS (
-	SELECT *,
-		(
-			CASE
-				WHEN Emp_Id = 5
-				AND Rol_Nombre LIKE 'Gerente de Operaciones' THEN 'JOP'
-				WHEN Emp_Id = 3
-				AND Rol_Nombre LIKE 'Coordinador de Operaciones' THEN 'JOP'
-				WHEN Rol_Nombre LIKE 'Jefe de Operaciones' THEN 'JOP'
-				ELSE ''
-			END
-		) AS Rol_Sucursal
-	FROM DL_FARINTER.DBO.DL_Kielsa_Seg_Rol -- {{ source('DL_FARINTER', 'DL_Kielsa_Seg_Rol') }}
-	WHERE --Emp_Id=3 AND Rol_Padre =7 and Rol_Nivel <10 --and Rol_Nombre LIKE '%operac%'
-		(
-			CASE
-				WHEN Emp_Id = 5
-				AND Rol_Nombre LIKE 'Gerente de Operaciones' THEN 1
-				WHEN Emp_Id = 3
-				AND Rol_Nombre LIKE 'Coordinador de Operaciones' THEN 1
-				WHEN Rol_Nombre LIKE 'Jefe de Operaciones' THEN 1
-				ELSE 0
-			END
-		) = 1
+    SELECT
+        *,
+        (
+            CASE
+                WHEN
+                    Emp_Id = 5
+                    AND Rol_Nombre LIKE 'Gerente de Operaciones' THEN 'JOP'
+                WHEN
+                    Emp_Id = 3
+                    AND Rol_Nombre LIKE 'Coordinador de Operaciones' THEN 'JOP'
+                WHEN Rol_Nombre LIKE 'Jefe de Operaciones' THEN 'JOP'
+                ELSE ''
+            END
+        ) AS Rol_Sucursal
+    FROM DL_FARINTER.DBO.DL_Kielsa_Seg_Rol -- {{ source('DL_FARINTER', 'DL_Kielsa_Seg_Rol') }}
+    WHERE --Emp_Id=3 AND Rol_Padre =7 and Rol_Nivel <10 --and Rol_Nombre LIKE '%operac%'
+        (
+            CASE
+                WHEN
+                    Emp_Id = 5
+                    AND Rol_Nombre LIKE 'Gerente de Operaciones' THEN 1
+                WHEN
+                    Emp_Id = 3
+                    AND Rol_Nombre LIKE 'Coordinador de Operaciones' THEN 1
+                WHEN Rol_Nombre LIKE 'Jefe de Operaciones' THEN 1
+                ELSE 0
+            END
+        ) = 1
 ),
+
 usuario_jop AS (
-	SELECT RJ.Emp_Id,
-		RJ.Rol_Id,
-		RJ.Rol_Nombre,
-		RJ.Rol_Sucursal,
-		U.Usuario_Email,
-		U.Usuario_Login,
-		U.Usuario_Nombre,
-		U.Usuario_Id
-	FROM rol_jop RJ
-		INNER JOIN DL_FARINTER.DBO.DL_Kielsa_Seg_Usuario_x_Rol UR -- {{ ref("DL_Kielsa_Seg_Usuario_x_Rol") }}
-		ON RJ.Rol_Id = UR.Rol_Id
-		AND RJ.Emp_Id = UR.Emp_Id
-		INNER JOIN DL_FARINTER.DBO.DL_Kielsa_Seg_Usuario U -- {{ ref("DL_Kielsa_Seg_Usuario") }}
-		ON U.Emp_Id = UR.Emp_Id
-		AND U.Usuario_Id = UR.Usuario_Id
-	WHERE U.Usuario_Eliminado = 0
-		AND U.Usuario_Cuenta_Deshabilitada = 0
+    SELECT
+        RJ.Emp_Id,
+        RJ.Rol_Id,
+        RJ.Rol_Nombre,
+        RJ.Rol_Sucursal,
+        U.Usuario_Email,
+        U.Usuario_Login,
+        U.Usuario_Nombre,
+        U.Usuario_Id
+    FROM rol_jop AS RJ
+    INNER JOIN DL_FARINTER.DBO.DL_Kielsa_Seg_Usuario_x_Rol AS UR -- {{ ref("DL_Kielsa_Seg_Usuario_x_Rol") }}
+        ON
+            RJ.Rol_Id = UR.Rol_Id
+            AND RJ.Emp_Id = UR.Emp_Id
+    INNER JOIN DL_FARINTER.DBO.DL_Kielsa_Seg_Usuario AS U -- {{ ref("DL_Kielsa_Seg_Usuario") }}
+        ON
+            UR.Emp_Id = U.Emp_Id
+            AND UR.Usuario_Id = U.Usuario_Id
+    WHERE
+        U.Usuario_Eliminado = 0
+        AND U.Usuario_Cuenta_Deshabilitada = 0
 ),
+
 usuario_supervisor AS (
-	SELECT RS.Emp_Id,
-		RS.Rol_Id,
-		RS.Rol_Nombre,
-		RS.Rol_Sucursal,
-		U.Usuario_Email,
-		U.Usuario_Login,
-		U.Usuario_Nombre,
-		U.Usuario_Id
-	FROM rol_supervisor RS
-		INNER JOIN DL_FARINTER.DBO.DL_Kielsa_Seg_Usuario_x_Rol UR -- {{ ref("DL_Kielsa_Seg_Usuario_x_Rol") }}
-		ON RS.Rol_Id = UR.Rol_Id
-		AND RS.Emp_Id = UR.Emp_Id
-		INNER JOIN DL_FARINTER.DBO.DL_Kielsa_Seg_Usuario U ON U.Emp_Id = UR.Emp_Id
-		AND U.Usuario_Id = UR.Usuario_Id
-	WHERE U.Usuario_Eliminado = 0
-		AND U.Usuario_Cuenta_Deshabilitada = 0 
+    SELECT
+        RS.Emp_Id,
+        RS.Rol_Id,
+        RS.Rol_Nombre,
+        RS.Rol_Sucursal,
+        U.Usuario_Email,
+        U.Usuario_Login,
+        U.Usuario_Nombre,
+        U.Usuario_Id
+    FROM rol_supervisor AS RS
+    INNER JOIN DL_FARINTER.DBO.DL_Kielsa_Seg_Usuario_x_Rol AS UR -- {{ ref("DL_Kielsa_Seg_Usuario_x_Rol") }}
+        ON
+            RS.Rol_Id = UR.Rol_Id
+            AND RS.Emp_Id = UR.Emp_Id
+    INNER JOIN DL_FARINTER.DBO.DL_Kielsa_Seg_Usuario AS U
+        ON
+            UR.Emp_Id = U.Emp_Id
+            AND UR.Usuario_Id = U.Usuario_Id
+    WHERE
+        U.Usuario_Eliminado = 0
+        AND U.Usuario_Cuenta_Deshabilitada = 0
 ),
+
 usuario_supervisor_sucursal AS (
-	SELECT SU.Emp_Id,
-		SU.Suc_Id,
-		US.Rol_Sucursal,
-		SU.Usuario_Id,
-		US.Rol_Id,
-		US.Rol_Nombre,
-		US.Usuario_Nombre,
-		SU.SxU_Fec_Actualizacion,
-		ROW_NUMBER() over (
-			PARTITION by SU.Emp_Id,
-			SU.Suc_Id
-			ORDER BY SU.SxU_Fec_Actualizacion DESC
-		) AS Orden_Asignacion
-	FROM usuario_supervisor US
-		INNER JOIN DL_FARINTER.DBO.DL_Kielsa_Seg_Sucursal_x_Usuario SU -- {{ ref('DL_Kielsa_Seg_Sucursal_x_Usuario') }}
-		ON us.Emp_Id = SU.Emp_id
-		AND us.Usuario_Id = SU.Usuario_Id
+    SELECT
+        SU.Emp_Id,
+        SU.Suc_Id,
+        US.Rol_Sucursal,
+        SU.Usuario_Id,
+        US.Rol_Id,
+        US.Rol_Nombre,
+        US.Usuario_Nombre,
+        SU.SxU_Fec_Actualizacion,
+        ROW_NUMBER() OVER (
+            PARTITION BY SU.Emp_Id,
+            SU.Suc_Id
+            ORDER BY SU.SxU_Fec_Actualizacion DESC
+        ) AS Orden_Asignacion
+    FROM usuario_supervisor AS US
+    INNER JOIN DL_FARINTER.DBO.DL_Kielsa_Seg_Sucursal_x_Usuario AS SU -- {{ ref('DL_Kielsa_Seg_Sucursal_x_Usuario') }}
+        ON
+            US.Emp_Id = SU.Emp_id
+            AND US.Usuario_Id = SU.Usuario_Id
 ),
+
 usuario_jop_sucursal AS (
-	SELECT SU.Emp_Id,
-		SU.Suc_Id,
-		US.Rol_Sucursal,
-		SU.Usuario_Id,
-		US.Rol_Id,
-		US.Rol_Nombre,
-		US.Usuario_Nombre,
-		SU.SxU_Fec_Actualizacion,
-		ROW_NUMBER() over (
-			PARTITION by SU.Emp_Id,
-			SU.Suc_Id
-			ORDER BY SU.SxU_Fec_Actualizacion DESC
-		) AS Orden_Asignacion
-	FROM usuario_jop US
-		INNER JOIN DL_FARINTER.DBO.DL_Kielsa_Seg_Sucursal_x_Usuario SU -- {{ ref('DL_Kielsa_Seg_Sucursal_x_Usuario') }}
-		ON us.Emp_Id = SU.Emp_id
-		AND us.Usuario_Id = SU.Usuario_Id
+    SELECT
+        SU.Emp_Id,
+        SU.Suc_Id,
+        US.Rol_Sucursal,
+        SU.Usuario_Id,
+        US.Rol_Id,
+        US.Rol_Nombre,
+        US.Usuario_Nombre,
+        SU.SxU_Fec_Actualizacion,
+        ROW_NUMBER() OVER (
+            PARTITION BY SU.Emp_Id,
+            SU.Suc_Id
+            ORDER BY SU.SxU_Fec_Actualizacion DESC
+        ) AS Orden_Asignacion
+    FROM usuario_jop AS US
+    INNER JOIN DL_FARINTER.DBO.DL_Kielsa_Seg_Sucursal_x_Usuario AS SU -- {{ ref('DL_Kielsa_Seg_Sucursal_x_Usuario') }}
+        ON
+            US.Emp_Id = SU.Emp_id
+            AND US.Usuario_Id = SU.Usuario_Id
 ),
+
 datos_finales AS (
-	SELECT *
-	FROM usuario_supervisor_sucursal
-	WHERE Orden_Asignacion = 1
-	UNION ALL
-	SELECT *
-	FROM usuario_jop_sucursal
-	WHERE Orden_Asignacion = 1
+    SELECT *
+    FROM usuario_supervisor_sucursal
+    WHERE Orden_Asignacion = 1
+    UNION ALL
+    SELECT *
+    FROM usuario_jop_sucursal
+    WHERE Orden_Asignacion = 1
 )
-SELECT *,
-	ISNULL(
-		{{ dwh_farinter_hash_column(unique_key_list) }},
-		''
-	) AS Hash_SucRolEmp,
-	GETDATE() AS [Fecha_Carga],
-	GETDATE() AS [Fecha_Actualizado]
+
+SELECT
+    *,
+    ISNULL(
+        {{ dwh_farinter_hash_column(unique_key_list) }},
+        ''
+    ) AS Hash_SucRolEmp,
+    GETDATE() AS [Fecha_Carga],
+    GETDATE() AS [Fecha_Actualizado]
 FROM datos_finales
--- dbt_dwh_farinter/models/dwh_kielsa/mart_datos_maestros/BI_Kielsa_Dim_Sucursal.sql
+++ dbt_dwh_farinter/models/dwh_kielsa/mart_datos_maestros/BI_Kielsa_Dim_Sucursal.sql
@@ -1,10 +1,12 @@
-
 {# Add dwh_farinter_remove_incremental_temp_table to all incremental models #}
 {# unique_key is accessible with config.get('unique_key') but it returns a string #}
 {# remember that macro here executes before the model is created, so we can't use it here #}
 {% set unique_key_list = ["Sucursal_Id","Emp_Id"] %}
-{# Post_hook can't access this context variables so we create the string here if needed only if the macros dont depende on query execution (just returns the query text) #}
-{#{% set post_hook_dwh_farinter_create_primary_key =  dwh_farinter_create_primary_key(this,columns=unique_key_list, create_clustered=False, is_incremental=0, show_info=True, if_another_exists_drop_it=True)  %}#}
+{# Post_hook can't access this context variables so we create the string here if
+needed only if the macros dont depende on query execution (just returns the query text) #}
+{#{% set post_hook_dwh_farinter_create_primary_key =  
+dwh_farinter_create_primary_key(this,columns=unique_key_list, create_clustered=False, 
+is_incremental=0, show_info=True, if_another_exists_drop_it=True)  %}#}
 {{ 
     config(
 		as_columnstore=true,
@@ -23,135 +25,147 @@
 		
 ) -}}
 
-WITH
-Sucursal_Numero AS
-(
-  SELECT
-    *, CAST(SUBSTRING(SNP2.Sucursal_Nombre, SNP2.Posicion_Inicial, SNP2.Posicion_Final) AS INT) AS [Sucursal_Numero]
-    , CAST(SUBSTRING(SNP2.Sucursal_Nombre, SNP2.Posicion_Inicial, SNP2.Posicion_Final) AS VARCHAR(50))  AS [Sucursal_Numero_Char]
-    , LEN(CAST(SUBSTRING(SNP2.Sucursal_Nombre, SNP2.Posicion_Inicial, SNP2.Posicion_Final) AS VARCHAR(50))) AS [Sucursal_Numero_Len]
-  FROM
-    (SELECT
-      *
-      , (LEN(Sucursal_Nombre) + 2 - Posicion_Inicial_Derecha) - Posicion_Inicial AS Longitud_Numero
-      , LEN(Sucursal_Nombre) Longitud_Nombre
+WITH --noqa: disable=RF02, RF03, ST06
+Sucursal_Numero AS (
+    SELECT
+        *,
+        CAST(SUBSTRING(SNP2.Sucursal_Nombre, SNP2.Posicion_Inicial, SNP2.Posicion_Final) AS INT) AS [Sucursal_Numero],
+        CAST(SUBSTRING(SNP2.Sucursal_Nombre, SNP2.Posicion_Inicial, SNP2.Posicion_Final) AS VARCHAR(50)) AS [Sucursal_Numero_Char],
+        LEN(CAST(SUBSTRING(SNP2.Sucursal_Nombre, SNP2.Posicion_Inicial, SNP2.Posicion_Final) AS VARCHAR(50))) AS [Sucursal_Numero_Len]
     FROM
-      (SELECT
-        *
-        , PATINDEX('%[0-9]%', [Sucursal_Nombre]) AS Posicion_Inicial
-        , PATINDEX('%[A-Z]%', SUBSTRING([Sucursal_Nombre], PATINDEX('%[0-9]%', [Sucursal_Nombre]) + 1, 50)) AS Posicion_Final
-        , PATINDEX('%[0-9]%', REVERSE([Sucursal_Nombre])) AS Posicion_Inicial_Derecha
-        , PATINDEX(
-          '%[A-Z]%'
-          , SUBSTRING(REVERSE([Sucursal_Nombre]), PATINDEX('%[0-9]%', REVERSE([Sucursal_Nombre])) + 1, 50)) AS Posicion_Final_Derecha
-      FROM
         (SELECT
-          [Sucursal_Id]
-          , [Emp_Id]
-          , 'A' + REPLACE(REPLACE(UPPER([Sucursal_Nombre]), ' ', ''), '-', '') + 'Z' AS [Sucursal_Nombre]
-        FROM	DL_FARINTER.dbo.DL_Kielsa_Sucursal -- {{ source('DL_FARINTER', 'DL_Kielsa_Sucursal') }}
-        ) SN ) SNP
-    WHERE Posicion_Inicial > 0) SNP2
+            *,
+            (LEN(Sucursal_Nombre) + 2 - Posicion_Inicial_Derecha) - Posicion_Inicial AS Longitud_Numero,
+            LEN(Sucursal_Nombre) AS Longitud_Nombre
+        FROM
+            (
+                SELECT
+                    *,
+                    PATINDEX('%[0-9]%', [Sucursal_Nombre]) AS Posicion_Inicial,
+                    PATINDEX('%[A-Z]%', SUBSTRING([Sucursal_Nombre], PATINDEX('%[0-9]%', [Sucursal_Nombre]) + 1, 50)) AS Posicion_Final,
+                    PATINDEX('%[0-9]%', REVERSE([Sucursal_Nombre])) AS Posicion_Inicial_Derecha,
+                    PATINDEX(
+                        '%[A-Z]%',
+                        SUBSTRING(REVERSE([Sucursal_Nombre]), PATINDEX('%[0-9]%', REVERSE([Sucursal_Nombre])) + 1, 50)
+                    ) AS Posicion_Final_Derecha
+                FROM
+                    (
+                        SELECT
+                            [Sucursal_Id],
+                            [Emp_Id],
+                            'A' + REPLACE(REPLACE(UPPER([Sucursal_Nombre]), ' ', ''), '-', '') + 'Z' AS [Sucursal_Nombre]
+                        FROM DL_FARINTER.dbo.DL_Kielsa_Sucursal -- {{ source('DL_FARINTER', 'DL_Kielsa_Sucursal') }}
+                    ) AS SN
+            ) AS SNP
+        WHERE Posicion_Inicial > 0) AS SNP2
 ),
-Sucursal_Numero_Fila as
-(
-  SELECT
-    *, ROW_NUMBER() OVER (PARTITION BY [Sucursal_Numero] ORDER BY [Sucursal_Id] DESC) AS [Fila]
-  FROM Sucursal_Numero
+
+Sucursal_Numero_Fila AS (
+    SELECT
+        *,
+        ROW_NUMBER() OVER (PARTITION BY [Sucursal_Numero] ORDER BY [Sucursal_Id] DESC) AS [Fila]
+    FROM Sucursal_Numero
 ),
-Sucursal_Numero_Fila_Unico as
-(
-  SELECT
-    *
-  FROM Sucursal_Numero_Fila
-  WHERE [Fila] = 1
+
+Sucursal_Numero_Fila_Unico AS (
+    SELECT *
+    FROM Sucursal_Numero_Fila
+    WHERE [Fila] = 1
 )
-SELECT ISNULL(S.[Sucursal_Id],0) AS [Sucursal_Id]
-        ,ISNULL(S.[Emp_Id],0) AS [Emp_Id]
-        ,[Version_Id]
-        ,[Version_Fecha]
-        ,S.[Sucursal_Nombre]
-        ,ISNULL(SN.[Sucursal_Numero],0) AS [Sucursal_Numero]
-        ,[Marca]
-        ,[Zona_Id]
-        ,{{ dwh_farinter_concat_key_columns(
+
+SELECT
+    ISNULL(S.[Sucursal_Id], 0) AS [Sucursal_Id],
+    ISNULL(S.[Emp_Id], 0) AS [Emp_Id],
+    S.[Version_Id],
+    S.[Version_Fecha],
+    S.[Sucursal_Nombre],
+    ISNULL(SN.[Sucursal_Numero], 0) AS [Sucursal_Numero],
+    S.[Marca],
+    S.[Zona_Id],
+    {{ dwh_farinter_concat_key_columns(
           columns=['Emp_Id', 'Zona_Id'], 
           input_length=49, 
-          table_alias='S')}} AS EmpZona_Id
-        ,CAST([Zona_Nombre] AS VARCHAR(100)) AS [Zona_Nombre]
-        ,[Departamento_Id]
-        ,{{ dwh_farinter_concat_key_columns(
+          table_alias='S') }} AS EmpZona_Id,
+    CAST(S.[Zona_Nombre] AS VARCHAR(100)) AS [Zona_Nombre],
+    S.[Departamento_Id],
+    {{ dwh_farinter_concat_key_columns(
           columns=['Emp_Id', 'Departamento_Id'], 
           input_length=49, 
-          table_alias='S')}} AS EmpDep_Id
-        ,[Departamento_Nombre]
-        ,[Municipio_Id]
-        ,{{ dwh_farinter_concat_key_columns(
+          table_alias='S') }} AS EmpDep_Id,
+    S.[Departamento_Nombre],
+    S.[Municipio_Id],
+    {{ dwh_farinter_concat_key_columns(
           columns=['Emp_Id', 'Departamento_Id', 'Municipio_Id'], 
           input_length=49, 
-          table_alias='S')}} AS EmpDepMun_Id
-        ,[Municipio_Nombre]
-        ,[Ciudad_Id]
-        ,{{ dwh_farinter_concat_key_columns(
+          table_alias='S') }} AS EmpDepMun_Id,
+    S.[Municipio_Nombre],
+    S.[Ciudad_Id],
+    {{ dwh_farinter_concat_key_columns(
           columns=['Emp_Id', 'Departamento_Id', 'Municipio_Id', 'Ciudad_Id'], 
           input_length=49, 
-          table_alias='S')}} AS EmpDepMunCiu_Id
-        ,[Ciudad_Nombre]
-        ,[TipoSucursal_Id]
-        ,[TipoSucursal_Nombre]
-        , CASE 
-          WHEN SN.[Sucursal_Numero_Char] IS NULL OR SN.[Sucursal_Numero_Len] = 0 
+          table_alias='S') }} AS EmpDepMunCiu_Id,
+    S.[Ciudad_Nombre],
+    S.[TipoSucursal_Id],
+    S.[TipoSucursal_Nombre],
+    CASE
+        WHEN SN.[Sucursal_Numero_Char] IS NULL OR SN.[Sucursal_Numero_Len] = 0
             THEN 'No Definido'
-          WHEN S.Emp_Id = 1 THEN CONCAT('kielsa',REPLICATE('0',3-SN.[Sucursal_Numero_Len])+SN.[Sucursal_Numero_Char],'@kielsa.hn') 
-          WHEN S.Emp_Id = 2 THEN CONCAT('h',SN.[Sucursal_Numero_Char],'@farmaciasherdez.com') 
-          WHEN S.Emp_Id = 3 THEN CONCAT('kielsa',REPLICATE('0',3-SN.[Sucursal_Numero_Len])+SN.[Sucursal_Numero_Char],'@kielsa.ni') 
-          WHEN S.Emp_Id = 4 THEN CONCAT('kielsa',REPLICATE('0',3-SN.[Sucursal_Numero_Len])+SN.[Sucursal_Numero_Char],'@kielsa.cr') 
-          WHEN S.Emp_Id = 5 THEN CONCAT('b',REPLICATE('0',3-SN.[Sucursal_Numero_Len])+SN.[Sucursal_Numero_Char],'@grupobrasilsv.com') 
-          ELSE 'No Definido' 
-          END AS [Correo_e]
-        ,[Direccion]
-        ,[Estado]
-        ,ISNULL(RSJ.[Usuario_Id], '0') as [Usuario_JOP_Id]
-        -- TODO: Migrar tablero de Jefrey a usar ID en lugar de nombres, implementar nuevamente el control por usuario.
-        ,CASE WHEN S.Emp_id = 1 then S.JOB  COLLATE DATABASE_DEFAULT ELSE  ISNULL(RSJ.[Usuario_Nombre], 'No Definido') END as [JOP]
-        ,ISNULL(RSS.[Usuario_Id], '0') as [Usuario_Supervisor_Id]
-        ,CASE WHEN S.Emp_id = 1 then S.Supervisor COLLATE DATABASE_DEFAULT ELSE  ISNULL(RSS.[Usuario_Nombre], 'No Definido') END as [Supervisor]
-        ,RSS.[Usuario_Nombre] as Usuario_Supervisor_Nombre
-        ,[Longitud]
-        ,[Latitud]
-        ,[CEDI_Id]
-        ,[Indicador_CEDI]
-        ,[Sucursal_Sinonimo] COLLATE DATABASE_DEFAULT AS [Sucursal_Sinonimo] 
-        ,[Hash_SucursalEmp]
-        ,[Hash_SucursalEmpVersion]
-        , {{ dwh_farinter_concat_key_columns(columns=['Emp_Id', 'Sucursal_Id'], input_length=19, table_alias='S')}} [EmpSuc_Id]
-        ,ABS(CAST(HASHBYTES('SHA1', 
-                        CONCAT([JOB],'')) AS bigint)) AS [Hash_JOP]
-        ,ABS(CAST(HASHBYTES('SHA1', 
-                        CONCAT([Supervisor],'')) AS bigint)) AS [Hash_Supervisor]
-        ,ISNULL({{ dwh_farinter_hash_column(["JOB"], table_alias='S' ) }},'') AS [HashStr_JOP]
-        ,ISNULL({{ dwh_farinter_hash_column(["Supervisor"], table_alias='S') }},'') AS [HashStr_Supervisor]
-        ,ISNULL({{ dwh_farinter_hash_column(["Usuario_Id"], table_alias='RSS') }},'') AS [HashStr_SupervisorId]
-        ,ISNULL({{ dwh_farinter_hash_column(unique_key_list, table_alias='S') }},'') AS [HashStr_SucEmp]
-        ,ISNULL({{ dwh_farinter_hash_column(unique_key_list+["Version_Id"], table_alias='S') }},'') AS [HashStr_SucEmpVersion]
-        ,LEFT(CONVERT(VARCHAR(32), HASHBYTES('MD5', CAST(CONCAT(S.Emp_Id,'-', S.Sucursal_Id) AS NVARCHAR(50))), 2), 32) as HashMD5_EmpSuc
-        ,ISNULL(CAST(GETDATE() AS DATETIME),'19000101') AS [Fecha_Carga]
-        ,ISNULL(CAST(GETDATE() AS DATETIME),'19000101') AS [Fecha_Actualizado]
-FROM DL_FARINTER.dbo.DL_Kielsa_Sucursal S -- {{ source('DL_FARINTER', 'DL_Kielsa_Sucursal') }} S
-LEFT JOIN Sucursal_Numero_Fila_Unico SN
-  ON S.Sucursal_Id = SN.Sucursal_Id
-  AND S.Emp_Id = SN.Emp_Id
-LEFT JOIN DL_FARINTER.dbo.DL_Kielsa_Sucursal_Rol_Usuario_Asignado RSJ -- {{ ref('DL_Kielsa_Sucursal_Rol_Usuario_Asignado') }} US
-  ON S.Emp_Id = RSJ.Emp_Id
-  AND S.Sucursal_Id = RSJ.Suc_Id
-  AND RSJ.Rol_Sucursal = 'JOP'
-LEFT JOIN DL_FARINTER.dbo.DL_Kielsa_Sucursal_Rol_Usuario_Asignado RSS -- {{ ref('DL_Kielsa_Sucursal_Rol_Usuario_Asignado') }} US
-  ON S.Emp_Id = RSS.Emp_Id
-  AND S.Sucursal_Id = RSS.Suc_Id
-  AND RSS.Rol_Sucursal = 'Supervisor'
+        WHEN S.Emp_Id = 1 THEN CONCAT('kielsa', REPLICATE('0', 3 - SN.[Sucursal_Numero_Len]) + SN.[Sucursal_Numero_Char], '@kielsa.hn')
+        WHEN S.Emp_Id = 2 THEN CONCAT('h', SN.[Sucursal_Numero_Char], '@farmaciasherdez.com')
+        WHEN S.Emp_Id = 3 THEN CONCAT('kielsa', REPLICATE('0', 3 - SN.[Sucursal_Numero_Len]) + SN.[Sucursal_Numero_Char], '@kielsa.ni')
+        WHEN S.Emp_Id = 4 THEN CONCAT('kielsa', REPLICATE('0', 3 - SN.[Sucursal_Numero_Len]) + SN.[Sucursal_Numero_Char], '@kielsa.cr')
+        WHEN S.Emp_Id = 5 THEN CONCAT('b', REPLICATE('0', 3 - SN.[Sucursal_Numero_Len]) + SN.[Sucursal_Numero_Char], '@grupobrasilsv.com')
+        ELSE 'No Definido'
+    END AS [Correo_e],
+    S.[Direccion],
+    S.[Estado],
+    ISNULL(RSJ.[Usuario_Id], '0') AS [Usuario_JOP_Id],
+    -- TODO: Migrar tablero de Jefrey a usar ID en lugar de nombres, implementar nuevamente el control por usuario.
+    CASE WHEN S.Emp_id = 1 THEN S.JOB COLLATE DATABASE_DEFAULT ELSE ISNULL(RSJ.[Usuario_Nombre], 'No Definido') END AS JOP,
+    ISNULL(RSS.[Usuario_Id], '0') AS [Usuario_Supervisor_Id],
+    CASE WHEN S.Emp_id = 1 THEN S.Supervisor COLLATE DATABASE_DEFAULT ELSE ISNULL(RSS.[Usuario_Nombre], 'No Definido') END AS [Supervisor],
+    RSS.[Usuario_Nombre] AS Usuario_Supervisor_Nombre,
+    S.[Longitud],
+    S.[Latitud],
+    S.[CEDI_Id],
+    S.[Indicador_CEDI],
+    S.[Sucursal_Sinonimo] COLLATE DATABASE_DEFAULT AS [Sucursal_Sinonimo],
+    S.[Hash_SucursalEmp],
+    S.[Hash_SucursalEmpVersion],
+    {{ dwh_farinter_concat_key_columns(columns=['Emp_Id', 'Sucursal_Id'], input_length=19, table_alias='S') }} AS [EmpSuc_Id],
+    ABS(CAST(HASHBYTES(
+        'SHA1',
+        CONCAT(S.JOB, '')
+    ) AS BIGINT)) AS [Hash_JOP],
+    ABS(CAST(HASHBYTES(
+        'SHA1',
+        CONCAT(S.[Supervisor], '')
+    ) AS BIGINT)) AS [Hash_Supervisor],
+    ISNULL({{ dwh_farinter_hash_column(["JOB"], table_alias='S' ) }}, '') AS [HashStr_JOP],
+    ISNULL({{ dwh_farinter_hash_column(["Supervisor"], table_alias='S') }}, '') AS [HashStr_Supervisor],
+    ISNULL({{ dwh_farinter_hash_column(["Usuario_Id"], table_alias='RSS') }}, '') AS [HashStr_SupervisorId],
+    ISNULL({{ dwh_farinter_hash_column(unique_key_list, table_alias='S') }}, '') AS [HashStr_SucEmp],
+    ISNULL({{ dwh_farinter_hash_column(unique_key_list+["Version_Id"], table_alias='S') }}, '') AS [HashStr_SucEmpVersion],
+    LEFT(CONVERT(VARCHAR(32), HASHBYTES('MD5', CAST(CONCAT(S.Emp_Id, '-', S.Sucursal_Id) AS NVARCHAR(50))), 2), 32) AS HashMD5_EmpSuc,
+    ISNULL(CAST(GETDATE() AS DATETIME), '19000101') AS [Fecha_Carga],
+    ISNULL(CAST(GETDATE() AS DATETIME), '19000101') AS [Fecha_Actualizado]
+FROM DL_FARINTER.dbo.DL_Kielsa_Sucursal AS S -- {{ source('DL_FARINTER', 'DL_Kielsa_Sucursal') }} S
+LEFT JOIN Sucursal_Numero_Fila_Unico AS SN
+    ON
+        S.Sucursal_Id = SN.Sucursal_Id
+        AND S.Emp_Id = SN.Emp_Id
+LEFT JOIN DL_FARINTER.dbo.DL_Kielsa_Sucursal_Rol_Usuario_Asignado AS RSJ -- {{ ref('DL_Kielsa_Sucursal_Rol_Usuario_Asignado') }} US
+    ON
+        S.Emp_Id = RSJ.Emp_Id
+        AND S.Sucursal_Id = RSJ.Suc_Id
+        AND RSJ.Rol_Sucursal = 'JOP'
+LEFT JOIN DL_FARINTER.dbo.DL_Kielsa_Sucursal_Rol_Usuario_Asignado AS RSS -- {{ ref('DL_Kielsa_Sucursal_Rol_Usuario_Asignado') }} US
+    ON
+        S.Emp_Id = RSS.Emp_Id
+        AND S.Sucursal_Id = RSS.Suc_Id
+        AND RSS.Rol_Sucursal = 'Supervisor'
 {% if is_incremental() %}
-  --WHERE S.Fecha_Actualizado >= coalesce((select max(Fecha_Actualizado) from {{ this }}), '19000101')
+--WHERE S.Fecha_Actualizado >= coalesce((select max(Fecha_Actualizado) from {{ this }}), '19000101')
 {% else %}
   --WHERE S.Fecha_Actualizado >= '19000101'
 {% endif %}
-
