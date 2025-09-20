f95bae70597c6e4dd3c3d78c103a129e06b16b1a
AxellPadilla <68310020+axellpadilla@users.noreply.github.com>
2025-09-20 23:19:23 +0000

    :sparkles: feat(bi_farinter): agregar modelo BI_Kielsa_Dim_TipoSucursal

--- dbt_dwh_farinter/models/dwh_kielsa/ldcom_etl_datos_maestros/DL_Kielsa_TipoSucursal.sql
+++ dbt_dwh_farinter/models/dwh_kielsa/ldcom_etl_datos_maestros/DL_Kielsa_TipoSucursal.sql
@@ -36,23 +36,22 @@ WHERE LS_LDCOM_RepLocal IS NOT NULL and Es_Empresa_Principal = 1
     {%- endif -%}
 {%- endfor -%}
 
-WITH DatosBase
-AS
-(
+WITH DatosBase AS (
 {%- for item in valid_empresas -%}
-{%- if not loop.first %}
-	UNION ALL{%- endif %}
-	SELECT ISNULL({{item['Empresa_Id']}},0) AS [Emp_Id]
-		, ISNULL(CAST(TipoSucursal_Id AS INT),0) AS TipoSucursal_Id
-		, TipoSucursal_Nombre COLLATE DATABASE_DEFAULT AS [TipoSucursal_Nombre]
-	FROM {{item['Servidor_Vinculado']}}.{{item['Base_Datos']}}.dbo.Tipo_Sucursal 
-	WHERE Emp_Id = {{item['Empresa_Id_Original']}} --AND Fecha_Actualizado >= {{last_date}}
-{% endfor -%}
+        {%- if not loop.first %}
+            UNION ALL{%- endif %}
+        SELECT
+            ISNULL({{ item['Empresa_Id'] }}, 0) AS [Emp_Id],
+            ISNULL(CAST(TipoSucursal_Id AS INT), 0) AS TipoSucursal_Id,
+            TipoSucursal_Nombre COLLATE DATABASE_DEFAULT AS [TipoSucursal_Nombre]
+        FROM {{ item['Servidor_Vinculado'] }}.{{ item['Base_Datos'] }}.dbo.Tipo_Sucursal
+        WHERE Emp_Id = {{ item['Empresa_Id_Original'] }} --AND Fecha_Actualizado >= {{ last_date }}
+    {% endfor -%}
 )
-SELECT *
-	, ABS(CAST(CAST(HASHBYTES('SHA2_256', CONCAT(TipoSucursal_Id, '-', Emp_Id)) AS INT) AS bigint))  AS Hash_TipoSucursalEmp 
-	, GETDATE() AS [Fecha_Carga]
-	, GETDATE() AS [Fecha_Actualizado]
+
+SELECT
+    *,
+    ABS(CAST(CAST(HASHBYTES('SHA2_256', CONCAT(TipoSucursal_Id, '-', Emp_Id)) AS INT) AS BIGINT)) AS Hash_TipoSucursalEmp,
+    GETDATE() AS [Fecha_Carga],
+    GETDATE() AS [Fecha_Actualizado]
 FROM datosBase
-
-
